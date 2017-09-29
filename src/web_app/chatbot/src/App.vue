5040d81b86695c2058f7924269784832e921afcb
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-09-28 22:16:39 -0400

    [#2] Add basic landing page and chat interface

--- src/web_app/chatbot/src/App.vue
+++ src/web_app/chatbot/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" id="logo">
     <router-view></router-view>
   </div>
 </template>
@@ -19,5 +19,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#logo {
+  height: 250px;
 }
 </style>
--- src/web_app/chatbot/src/components/Hello.vue
+++ src/web_app/chatbot/src/components/Hello.vue
@@ -1,53 +1,83 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="welcome">
+    <div class="landing" v-if="!startChat">
+      <h1>{{ msg }}</h1>
+    </div>
+    <div class="conversation" v-if="startChat">
+      <div class="convBlock">
+        <img class="avatar" src="../assets/user.png">
+        <p class="conversationMsg">{{ userMsg }}</p>
+      </div>
+      <div class="convBlock">
+        <img class="avatar" src="../assets/bot.png">
+        <p class="conversationMsg">{{ aiMsg }}</p>
+      </div>
+    </div>
+    <div><input type="text" v-model="userMsg"/></div>
+    <button v-on:click="openChatBot">Start</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
+  name: 'welcome',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to JusticeAI! To get started, please tell us what you’d like to do.',
+      startChat: false,
+      aiMsg: 'Hello, I am JusticeAI. Please let me take a look at your case.'
+    }
+  },
+  methods: {
+    openChatBot: function() {
+      this.startChat = true;
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1 {
   font-weight: normal;
+  font-size: 20px;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
+input {
+  width: 400px;
+  height: 25px;
+  font-size: 20px;
+  padding: 15px;
 }
-
-li {
+button {
+  border: none;
+  margin-top: 10px;
+  width: 185px;
+  height: 30px;
+  background-color: #5196C3;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: bold;
+}
+button:hover{
+  border-style: groove;
+  border: 1px, solid, #5196c3;
+  background-color: #ffffff;
+  color: #5196C3;
+}
+.avatar {
+  height: 70px;
+}
+.conversation {
+  margin-bottom: 30px;
+}
+.conversationMsg {
   display: inline-block;
-  margin: 0 10px;
+  margin-left: 50px;
+  position: absolute;
+  padding-top: 10px
 }
-
-a {
-  color: #42b983;
+.convBlock {
+  width: 70%;
+  margin: auto;
+  margin-top: 10px;
+  text-align: left;
 }
 </style>
