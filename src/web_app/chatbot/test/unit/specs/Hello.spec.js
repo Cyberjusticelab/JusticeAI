637bb9f333f769bf32ebca7849aaa45dee9b001e
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-09-28 22:18:54 -0400

    [#2] update unit test

--- src/web_app/chatbot/test/unit/specs/Hello.spec.js
+++ src/web_app/chatbot/test/unit/specs/Hello.spec.js
@@ -5,7 +5,7 @@ describe('Hello.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(Hello)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+    expect(vm.$el.querySelector('.welcome h1').textContent)
+      .to.equal('Welcome to JusticeAI! To get started, please tell us what you’d like to do.')
   })
 })
