644fcfe894c2e382f97a66cdc376b7221256ac26
Lance Lafontaine <lancelafontaine@protonmail.com>
2017-09-30 18:06:49 -0400

    [#2] Adds unit tests for chatbot functionality

--- src/web_app/test/unit/specs/Hello.spec.js
+++ src/web_app/test/unit/specs/Hello.spec.js
@@ -8,4 +8,95 @@ describe('Hello.vue', () => {
     expect(vm.$el.querySelector('.landing h1').textContent)
       .to.equal('Hi! I\'m ProceZeus.')
   })
+
+  it('should have default component data', () => {
+    const Constructor = Vue.extend(Hello)
+    const vm = new Constructor()
+
+    expect(vm.landingPageTitle).to.be.a('string')
+    expect(vm.landingPageMessage).to.be.a('string')
+    expect(vm.landingPagePrompt).to.be.a('string')
+    expect(vm.botName).to.be.a('string')
+    expect(vm.startChat).to.equal(false)
+    expect(vm.botMessageLog).to.be.a('array')
+    expect(vm.userMessageLog).to.be.a('array')
+  })
+
+  it('openChatBot should push a default message into the bot message queue', () => {
+    const Constructor = Vue.extend(Hello)
+    const vm = new Constructor().$mount()
+
+    vm.username = 'Bob'
+    expect(vm.startChat).to.equal(false)
+    expect(vm.botMessageLog.length).to.equal(0)
+
+    vm.openChatBot()
+
+    expect(vm.startChat).to.equal(true)
+    expect(vm.botMessageLog.length).to.equal(1)
+  })
+
+  it('openChatBot should push a default message into the bot message queue', () => {
+    const Constructor = Vue.extend(Hello)
+    const vm = new Constructor().$mount()
+
+    vm.username = 'Bob'
+    expect(vm.startChat).to.equal(false)
+    expect(vm.botMessageLog.length).to.equal(0)
+
+    vm.openChatBot()
+
+    expect(vm.startChat).to.equal(true)
+    expect(vm.botMessageLog.length).to.equal(1)
+  })
+
+  it('openChatBot should not push a default message into the bot message queue if the provided username is empty', () => {
+    const Constructor = Vue.extend(Hello)
+    const vm = new Constructor().$mount()
+
+    vm.username = '   '
+    expect(vm.startChat).to.equal(false)
+    expect(vm.botMessageLog.length).to.equal(0)
+
+    vm.openChatBot()
+
+    expect(vm.startChat).to.equal(false)
+    expect(vm.botMessageLog.length).to.equal(0)
+  })
+
+  it('submitUserText should add a message to the user and bot message queues and clear the text area', () => {
+    const Constructor = Vue.extend(Hello)
+    const vm = new Constructor().$mount()
+
+    vm.userTextInput = 'hotdogs'
+    expect(vm.userTextInput).to.equal('hotdogs')
+    expect(vm.botMessageLog.length).to.equal(0)
+    expect(vm.userMessageLog.length).to.equal(0)
+
+    vm.submitUserText()
+
+    expect(vm.userTextInput).to.equal('')
+    expect(vm.botMessageLog.length).to.equal(1)
+    expect(vm.userMessageLog.length).to.equal(1)
+  })
+
+  it('submitUserText should not add a message to the user or bot message queues and not clear the text area', () => {
+    const Constructor = Vue.extend(Hello)
+    const vm = new Constructor().$mount()
+
+    vm.userTextInput = '   '
+    expect(vm.userTextInput).to.equal('   ')
+    expect(vm.botMessageLog.length).to.equal(0)
+    expect(vm.userMessageLog.length).to.equal(0)
+
+    vm.submitUserText()
+
+    expect(vm.userTextInput).to.equal('   ')
+    expect(vm.botMessageLog.length).to.equal(0)
+    expect(vm.userMessageLog.length).to.equal(0)
+  })
+
+
 })
+
+
