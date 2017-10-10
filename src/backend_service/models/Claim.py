de59d43c0423dedb7c64f82e4937bdfb24eccb63
mihaiqc <mihai_qc@hotmail.fr>
2017-10-10 14:13:15 -0400

    Redefining Claim
    Github Issue #20 with @Vynny.

--- src/backend_service/models/Claim.py
+++ src/backend_service/models/Claim.py
@@ -1,6 +1,17 @@
 import uuid
 
 
+'''class Question(object):
+    def __init__(self):
+        self.answers = []
+
+    def get_answers(self):
+        return self.answers
+
+    def add_answer(self, answer):
+        self.answers.append(answer)'''
+
+
 class Claim(object):
     def __init__(self, name):
         self.id = str(uuid.uuid4())
@@ -9,12 +20,53 @@ class Claim(object):
         self.facts = []
         self.questions = []
         self.answers = []
+        self.counter = 0
+
+    def delete_last_set(self):
+        self.counter = (self.counter-1)
+
+    def get_counter(self):
+        return self.counter
 
     def set_person(self, person):
         self.person = person
+
+    def set_name__(self, name):
+        self.name = name
+
+    def add_fact(self, fact):
+        self.facts.append(fact)
+        self.counter = (self.counter+1)
 
     def add_question(self, question):
         self.questions.append(question)
 
     def add_answer(self, answer):
         self.answers.append(answer)
+
+    def get_name(self):
+        return self.name
+
+    def get_person(self):
+        return self.person
+
+    def get_facts(self):
+        return self.facts
+
+    def get_id(self):
+        return self.id
+
+    def get_specific_fact(self, row):
+        return self.facts[row]
+
+    def get_specific_question(self, row):
+        return self.questions[row]
+
+    def get_specific_answer(self, row):
+        return self.answers[row]
+
+    def get_questions(self):
+        return self.questions
+
+    def get_answers(self):
+        return self.answers
