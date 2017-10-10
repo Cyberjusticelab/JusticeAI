d9aed04a0784f4ad3235c74eb1641d9b2dd3c874
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-10-10 00:13:18 -0400

    [#15] fix styling

--- src/web_client/src/components/Chat.vue
+++ src/web_client/src/components/Chat.vue
@@ -1,20 +1,25 @@
 <template>
-  <div id="chat-component">
-    <div id="chat-widow" v-chat-scroll>
-      <div class="chat-message" v-for="(message, index) in chatLog">
-        <div v-bind:class="{ 'chat-message-user': message.type == 'user', 'chat-message-zeus': message.type == 'zeus' }">
-          <img v-if="message.type == 'zeus'" src="../assets/bot.png"/>
-          <p>{{ message.text }}</p>
+  <b-container>
+    <b-row>
+      <b-col cols="12" sm="10">
+        <div id="chat-component">
+          <div id="chat-widow" v-chat-scroll>
+            <div class="chat-message" v-for="(message, index) in chatLog">
+              <div v-bind:class="{ 'chat-message-user': message.type == 'user', 'chat-message-zeus': message.type == 'zeus' }">
+                <img v-if="message.type == 'zeus'" src="../assets/bot.png"/>
+                <p>{{ message.text }}</p>
+              </div>
+            </div>
+          </div>
+          <div id="chat-input">
+            <input id="chat-input-text" type="textarea" v-model="currentUserInput"/>
+            <button id="chat-input-submit" v-on:click="sendUserMessage" :disabled="!currentUserInput">Send</button>
+            <icon id="chat-input-voice" name="microphone" scale="3"></icon>
+          </div>
         </div>
-      </div>
-    </div>
-    <div id="chat-window-bottom"></div>
-    <div id="chat-input">
-      <input type="textarea" v-model="currentUserInput"/>
-      <button v-on:click="sendUserMessage" :disabled="!currentUserInput">Send</button>
-      <button>Voice</button>
-    </div>
-  </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -22,7 +27,7 @@ export default {
   data () {
     return {
       chatLog: new Array,
-      currentUserInput: new String
+      currentUserInput: undefined
     }
   },
   created: function() {
@@ -39,7 +44,7 @@ export default {
         type: 'user',
         text: this.currentUserInput
       });
-      this.currentUserInput = new String;
+      this.currentUserInput = undefined;
     }
   }
 }
@@ -47,28 +52,46 @@ export default {
 
 <style lang="scss" scoped>
 #chat-widow {
+  margin-top: 20px;
   height: 800px;
   border: 1px solid #000000;
   overflow-y: scroll;
-}
-.chat-message {
-  height: 20px;
-  margin: 0px 20px 0px 20px;
-}
-.chat-message-user {
-  @extend .chat-message;
-  text-align: right;
-
-}
-.chat-message-zeus {
-  @extend .chat-message;
-  text-align: left;
-  img {
-    width: 20px;
-    display: inline-block;
+  .chat-message {
+    height: 20px;
+    margin: 20px 20px 0px 20px;
+    font-size:35px;
   }
-  p {
-    display: inline-block;
+  .chat-message-user {
+    @extend .chat-message;
+    margin-top: 36px;
+    text-align: right;
+  }
+  .chat-message-zeus {
+    @extend .chat-message;
+    text-align: left;
+    img {
+      width: 60px;
+      margin-right: 20px;
+      display: inline-block;
+    }
+    p {
+      display: inline-block;
+    }
+  }
+}
+#chat-input {
+  margin-top: 20px;
+  #chat-input-text {
+    width: 500px;
+    height: 50px;
+    padding: 2px 20px 2px 20px;
+  }
+  #chat-input-submit {
+    height: 58px;
+    width: 110px;
+  }
+  #chat-input-voice {
+    margin: 0px 0px -18px 20px;
   }
 }
 </style>
--- src/web_client/src/main.js
+++ src/web_client/src/main.js
@@ -3,12 +3,15 @@ import ProceZeus from './ProceZeus'
 import router from './router'
 import VueChatScroll from 'vue-chat-scroll'
 import BootstrapVue from 'bootstrap-vue'
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(VueChatScroll)
 Vue.use(BootstrapVue)
+Vue.component('icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
