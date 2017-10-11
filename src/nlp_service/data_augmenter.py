4bbc69437f0befb23d84a2a0492b169b9206e75f
Arek Mano <manoukian.arek@gmail.com>
2017-10-10 22:19:17 -0400

    [#8] More lint issues fixes. One day, I will look back and laugh, but it is not this day

--- src/nlp_service/data_augmenter.py
+++ src/nlp_service/data_augmenter.py
@@ -6,32 +6,35 @@ from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
 import nltk
 
-inputs = ['rent_change', 'lease_termination', 'deposits', 'nonpayment', 'tenant', 'landlord']
+inputs = ['rent_change', 'lease_termination',
+          'deposits', 'nonpayment', 'tenant', 'landlord']
 
 for inp in inputs:
-  f = open('data/' + inp + '.txt')
-  samples = set([l.strip('\n') for l in f.readlines()])
+    f = open('data/' + inp + '.txt')
+    samples = set([l.strip('\n') for l in f.readlines()])
 
-  expanded = set()
-  stoppers = set(stopwords.words('english'))
-  print(inp + ' old size: ' + str(len(samples)))
-  while len(expanded) != len(samples):
-    for sample in samples:
-      sample = sample.lower()
-      words = nltk.word_tokenize(sample)
-      for i in words:
-        if i not in stoppers:
-          synsets = wn.synsets(i)
-          commons = set([synset.name().split(".")[0] for synset in synsets])
-          if len(commons) > 0:
-            for hyponym in commons:
-              newSample = sample.replace(i, hyponym.replace("_", " "))
-              expanded.add(newSample)
-    samples = expanded.copy()
+    expanded = set()
+    stoppers = set(stopwords.words('english'))
+    print(inp + ' old size: ' + str(len(samples)))
+    while len(expanded) != len(samples):
+        for sample in samples:
+            sample = sample.lower()
+            words = nltk.word_tokenize(sample)
+            for i in words:
+                if i not in stoppers:
+                    synsets = wn.synsets(i)
+                    commons = set([synset.name().split(".")[0]
+                                   for synset in synsets])
+                    if len(commons) > 0:
+                        for hyponym in commons:
+                            newSample = sample.replace(
+                                i, hyponym.replace("_", " "))
+                            expanded.add(newSample)
+        samples = expanded.copy()
 
-  # print(samples)
-  print(inp + ' new size: ' + str(len(expanded)))
+    # print(samples)
+    print(inp + ' new size: ' + str(len(expanded)))
 
-  out = open('data/' + inp + '.extended.txt', 'w')
-  for item in expanded:
-    out.write(item + '\n')
+    out = open('data/' + inp + '.extended.txt', 'w')
+    for item in expanded:
+        out.write(item + '\n')
