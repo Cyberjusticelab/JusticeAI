bc95afe27529806b18db054d8c75d01fb55360cf
Arek Mano <manoukian.arek@gmail.com>
2017-10-10 22:16:53 -0400

    [#8] Fixing lint issues. Despair has taken over

--- src/nlp_service/services/problem_category_classifier.py
+++ src/nlp_service/services/problem_category_classifier.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-from textblob import TextBlob
 from models.question import QuestionOutput
 from services.gram_classifier import GramClassifier
-import nltk
-import random
 
 class ProblemCategoryClassifier(GramClassifier):
     inputFiles = ['deposits', 'lease_termination', 'nonpayment', 'rent_change']
+
     """docstring for ProblemCategoryClassifier"""
     def __init__(self):
         super().__init__(ProblemCategoryClassifier.inputFiles)
