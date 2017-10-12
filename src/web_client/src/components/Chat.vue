5b1d979ea356f26dc25a22aeb33b4be9bead0d0d
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-10-11 23:49:06 -0400

    [#15] implement rest calls and chat logic based on existing api doc

--- src/web_client/src/components/Chat.vue
+++ src/web_client/src/components/Chat.vue
@@ -1,12 +1,16 @@
+<style lang="scss" scoped>
+@import "../theme/Chat"
+</style>
+
 <template>
   <b-container>
     <b-row>
-      <b-col cols="12" sm="10">
+      <b-col cols="12" sm="12">
         <div id="chat-component">
           <div id="chat-widow" v-chat-scroll>
             <div class="chat-message" v-for="(message, index) in chatLog">
-              <div v-bind:class="{ 'chat-message-user': message.type == 'user', 'chat-message-zeus': message.type == 'zeus' }">
-                <img v-if="message.type == 'zeus'" src="../assets/bot.png"/>
+              <div v-bind:class="{ 'chat-message-user': message.type == 'USER', 'chat-message-zeus': message.type == 'BOT' }">
+                <img v-if="message.type == 'BOT'" src="../assets/bot.png"/>
                 <p>{{ message.text }}</p>
               </div>
             </div>
@@ -27,43 +31,67 @@ export default {
   data () {
     return {
       chatLog: new Array,
-      currentUserInput: undefined
+      currentUserInput: null,
+      username: null,
+      connectionError: false
     }
   },
   created: function() {
-    //TODO: init session
-    this.chatLog.push({
-      type: 'zeus',
-      text: 'HI!'
-    });
+    if (!this.$localStorage.get('zeusId')) {
+      const zeusId = this.$localStorage.get('zeusId');
+      this.getChatHistory(zeusId, this.initChat);
+    } else {
+      this.initChat();
+    }
   },
   methods: {
-    sendUserMessage: function() {
-      // TODO: request to APIs to send the message first, then do callback to push the object
-      this.$http.get('http://www.whatever.com').then(response => {
-        this.zeusResponse = response.body;
-        this.showMessage(this.currentUserInput, 'user');
-        // TODO: push zeus message to chatlog
-        // TODO: play around with the data
-        console.log(this.zeusResponse);
-      }, response => {
-        this.showMessage(this.currentUserInput, 'user');
-        // error callback
+    initChat: function() {
+      this.chatLog.push({
+        type: 'BOT',
+        text: 'Hello there, I am ProceZeus, your personal legal assisstant. To get started, can you kindly tell me what is your name?'
       });
     },
+    initChatSession: function() {
+      this.$http.post('http://localhost:3003/new',{
+        name: this.username
+      }).then(response => {
+        this.$localStorage.set('zeusId', response.body.conversation_id);
+      }, response => {
+        this.connectionError = true;
+      });
+    },
+    sendUserMessage: function() {
+      this.$http.post('http://localhost:3003/conversation', {
+        conversation_id: this.$localStorage.get('zeusId'),
+        message: this.currentUserInput
+      }).then(response => {
+        this.showMessage(this.currentUserInput, 'USER');
+        this.showMessage(response.body.message, 'BOT');
+      }, response => {
+        this.connectionError = true;
+      });
+    },
+    getChatHistory: function(zeusId, callback) {
+      this.$http.get('http://localhost:3003/conversation/' + zeusId).then(response => {
+        var chatHistory = response.body.message;
+        for (var i = 0; i < chatHistory.length; i++) {
+          this.showMessage(chatHistory[i].text, chatHistory[i].sender_type);
+        }
+        this.username = response.body.name;
+      }, response => {
+        callback();
+      });
+    },
+    // push message to chatlog
     showMessage: function(text, type) {
       this.chatLog.push({
         text: text,
         type: type
       });
-      if (type === 'user') {
+      if (type === 'USER') {
         this.currentUserInput = undefined;
       }
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-@import "../theme/Chat"
-</style>
--- src/web_client/src/main.js
+++ src/web_client/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import ProceZeus from './ProceZeus'
 import router from './router'
 import VueResource from 'vue-resource'
+import VueLocalStorage from 'vue-localstorage'
 import VueChatScroll from 'vue-chat-scroll'
 import BootstrapVue from 'bootstrap-vue'
 import Icon from 'vue-awesome/components/Icon'
@@ -11,6 +12,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(VueResource);
+Vue.use(VueLocalStorage)
 Vue.use(VueChatScroll)
 Vue.use(BootstrapVue)
 Vue.component('icon', Icon)
