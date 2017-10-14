5d1128e89c60f4bde0248554b0d1af0317d152b2
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-10-14 04:46:45 -0400

    [#15] integrated with back-end, test, restructure template, apply initial theme, and deliver functional chat component

--- src/web_client/src/ProceZeus.vue
+++ src/web_client/src/ProceZeus.vue
@@ -1,3 +1,7 @@
+<style lang="scss" scoped>
+@import "./theme/Main"
+</style>
+
 <template>
   <div id="proceZeus">
     <router-view></router-view>
@@ -9,22 +13,3 @@ export default {
   name: 'proceZeus'
 }
 </script>
-
-<style>
-html, body {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-#proceZeus {
-  position: relative;
-  min-width: 100%;
-  min-height: 100%;
-  text-align: center;
-}
-</style>
--- src/web_client/src/components/Chat.vue
+++ src/web_client/src/components/Chat.vue
@@ -3,19 +3,34 @@
 </style>
 
 <template>
-  <b-container id="chat-component">
+  <b-container fluid id="chat-component">
+    <b-row id="chat-title">
+      <b-col sm="2"><h2>ProceZeus</h2></b-col>
+    </b-row>
     <div id="chat-widow" v-chat-scroll>
-      <div class="chat-message" v-for="(message, index) in chatLog">
-        <div v-bind:class="{ 'chat-message-user': message.type == 'USER', 'chat-message-zeus': message.type == 'BOT' }">
-          <img v-if="message.type == 'BOT'" src="../assets/bot.png"/>
-          <p>{{ message.text }}</p>
-        </div>
+      <div class="chat-message-container" v-for="(message, index) in chatLog">
+        <b-row>
+          <b-col sm="3" offset-sm="1">
+            <div v-if="message.type == 'BOT'" class="chat-message-img">
+              <img src="../assets/zeus.jpg"/>
+            </div>
+          </b-col>
+          <b-col sm="8">
+            <div v-bind:class="{ 'chat-message-user': message.type == 'USER', 'chat-message-zeus': message.type == 'BOT' }" class="chat-message-text">
+              <p>{{ message.text }}</p>
+            </div>
+          </b-col>
+        </b-row>
       </div>
     </div>
     <div id="chat-input">
-      <input id="chat-input-text" type="textarea" v-model="currentUserInput"/>
-      <button id="chat-input-submit" v-on:click="sendUserMessage" :disabled="!currentUserInput">Send</button>
-      <icon id="chat-input-voice" name="microphone" scale="3"></icon>
+      <b-form @submit="sendUserMessage">
+        <b-form-group>
+          <b-form-input id="chat-input-text" v-model="currentUserInput" placeholder="Enter your message" autocomplete="off"></b-form-input>
+          <b-button id="chat-input-submit" size="lg" variant="outline-success" type="submit":disabled="!currentUserInput">Send</b-button>
+        </b-form-group>
+      </b-form>
+      <!--<icon id="chat-input-voice" name="microphone" scale="3"></icon>-->
     </div>
   </b-container>
 </template>
@@ -26,7 +41,7 @@ export default {
     return {
       chatLog: new Array,
       currentUserInput: null,
-      username: 'Hammer of Justice', // TODO: prompt user's name
+      username: 'Hammer of Justice!', // TODO: prompt user's name
       connectionError: false
     }
   },
@@ -45,21 +60,21 @@ export default {
       }).then(
         response => {
           this.$localStorage.set('zeusId', response.body.conversation_id);
-          sendUserMessage('');
+          this.currentUserInput = '';
+          this.sendUserMessage();
         },
         response => {
           this.connectionError = true;
         }
       );
     },
-    sendUserMessage (message) {
-      let userMessage = message || this.currentUserInput;
+    sendUserMessage () {
       this.$http.post('http://localhost:3003/conversation', {
         conversation_id: this.$localStorage.get('zeusId'),
-        message: userMessage
+        message: this.currentUserInput
       }).then(
         response => {
-          this.shwoMessage(this.currentUserInput, 'USER');
+          this.showMessage(this.currentUserInput, 'USER');
           this.showMessage(response.body.message, 'BOT');
         },
         response => {
@@ -70,7 +85,7 @@ export default {
     getChatHistory (zeusId) {
       this.$http.get('http://localhost:3003/conversation/' + zeusId).then(
         response => {
-          let chatHistory = response.body.message;
+          let chatHistory = response.body.messages;
           for (let i = 0; i < chatHistory.length; i++) {
             this.showMessage(chatHistory[i].text, chatHistory[i].sender_type);
           }
@@ -81,12 +96,13 @@ export default {
         }
       );
     },
-    // push message to chatlog
     showMessage (text, type) {
-      this.chatLog.push({
-        text: text,
-        type: type
-      });
+      if (text) {
+        this.chatLog.push({
+          text: text,
+          type: type
+        });
+      }
       if (type === 'USER') {
         this.currentUserInput = undefined;
       }
