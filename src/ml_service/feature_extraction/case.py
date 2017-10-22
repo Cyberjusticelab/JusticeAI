064dffc8c12f05df0d9c1608c22fd85c8bbbb3eb
taimoorrana <taimoorrana1@gmail.com>
2017-10-22 19:17:10 -0400

    [#68] Can now capture multiple 'no dossier' and 'no demande'

--- src/ml_service/feature_extraction/case.py
+++ src/ml_service/feature_extraction/case.py
@@ -18,27 +18,49 @@ class Case:
     def _open_file(self, file_path):
         current_file = codecs.open(file_path, 'r', "iso-8859-1")
 
+        if file_name == "AZ-51145180.txt":
+            test = "gsdgs"
         for line in current_file:
 
             # Extract "No dossier" ID
             if self.no_dossier == 0 and re.search("^[N|n]o[s]? dossier[s]?\s*:", line):
-                next_line = current_file.next()
+                line = current_file.next().strip()
 
+                while line == "":
+                    line = current_file.next().strip()
+
+                count = 0
+                data = []
                 # if the next line contains numbers then it's the ID we are looking for
-                if re.search("^[0-9]", next_line) is not None:
-                    self.no_dossier = next_line
-                    self.data["noDossier"] = next_line.replace("\r", "")
-                    continue
+                while re.search("^[0-9]{2}.+[A-Z]$", line) is not None:
+                    self.no_dossier = line.strip()
+                    data.append(line.strip())
+                    line = current_file.next().strip()
+                    count += 1
+
+                if count > 1:
+                    print file_name
+                self.data["noDossier"] = data
 
             # Extract "No demande" ID
             if self.no_demande == 0 and re.search("^[N|n]o[s]? demande[s]?\s*:", line):
-                next_line = current_file.next()
+                line = current_file.next().strip()
 
+                while line == "":
+                    line = current_file.next().strip()
                 # if the next line contains numbers then it's the ID we are looking for
-                if re.search("^[0-9]", next_line) is not None:
-                    self.no_demande = next_line
-                    self.data["noDemande"] = next_line.replace("\r", "")
-                    continue
+                data = []
+                count = 0
+
+                while re.search("^[0-9].*[0-9]$", line) is not None:
+                    self.no_demande = line.strip()
+                    for id in line.strip().split(" et "):
+                        data.append(id)
+                    line = current_file.next().strip()
+                    count += 1
+
+                self.data["noDemande"] = data
+
             # Extract total amount of hearings
             # Each hearing has a date stamp. Counting each date stamp give us the total amount of hearing for this case
             if re.search("^[D|d]ate\s*:", line):
@@ -52,12 +74,13 @@ class Case:
         if self.total_hearings > 1:
             self.is_rectified = True
             self.data["isRectified"] = True
-
+        else:
+            self.data["isRectified"] = False
         self.data["totalHearings"] = self.total_hearings
 
 
 def writeToJSONFile(path, file_name, data):
-    with open(path + '/' + file_name + ".json","w+") as JSON_File:
+    with open(path + '/' + file_name + ".json", "w+") as JSON_File:
         json.dump(data, JSON_File)
 
 
@@ -66,5 +89,5 @@ directory_path = "/Users/taimoorrana/Downloads/text_bk/"
 for file_name in os.listdir(directory_path):
     if file_name != ".DS_Store":
         case = Case(directory_path + file_name)
-        writeToJSONFile('/Users/taimoorrana/Desktop/testJson', "case#"+case.no_demande, case.data)
-        print file_name, ": ", case.total_hearings
+        writeToJSONFile('/Users/taimoorrana/Desktop/testJson', "case#" + case.no_demande, case.data)
+        print file_name, ": ", case.data["noDossier"], case.data["noDemande"]
