a79c8096eeec5110bf5c1e15efcc1088c986273a
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-10-24 15:53:47 -0400

    5/create dashboard component, create template sidebar component for future use, assemble chat component

---
+++ src/web_client/src/components/Dashboard.vue
@@ -0,0 +1,28 @@
+<style lang="scss" scoped>
+@import "../theme/Dashboard"
+</style>
+
+<template>
+    <div id="dashboard-component">
+        <b-container fluid>
+            <b-row>
+                <b-col md="3">
+                    <sidebar></sidebar>
+                </b-col>
+                <b-col md="9">
+                    <chat></chat>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+import Chat from './Chat'
+import Sidebar from './Sidebar'
+export default {
+    components: {
+        Chat, Sidebar
+    }
+}
+</script>
---
+++ src/web_client/src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<style lang="scss" scoped>
+@import "../theme/Sidebar"
+</style>
+
+<template>
+    <div id="sidebar-component">
+        <p>Hey</p>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
