7372fc7dca0bec14d4dfff1d7003d6ead5e927b8
taimoorrana <taimoorrana1@gmail.com>
2017-10-24 16:01:43 -0400

    [#68] added tests

---
+++ src/ml_service/feature_extraction/case_test.py
@@ -0,0 +1,21 @@
+import case
+
+case = case.Case("./dummy_cases/sample_case1.txt")
+
+
+def test_no_demande():
+    assert len(case.data["noDemande"]) == 1
+    assert case.data["noDemande"][0] == "1234567"
+
+
+def test_no_dossier():
+    assert len(case.data["noDossier"]) == 1
+    assert case.data["noDossier"][0] == "123456 12 12345678 D"
+
+
+def test_total_hearings():
+    assert case.data["totalHearings"] == 2
+
+
+def test_is_rectified():
+    assert case.data["isRectified"]
