f6289f9d8400092c273c8643b911ffa52820f6b9
samuel-campbell <samuel.pcampbell@gmail.com>
2017-11-01 14:47:33 -0400

    [#105] removed training data

--- src/ml_service/preprocessing/train.py
+++
@@ -1,8 +0,0 @@
-from src.ml_service.preprocessing.French.NerTraining import Ner
-
-'''
-Look at /French/GlobalVariables/ to set your models directory
-'''
-if __name__ == '__main__':
-    ner = Ner()
-    ner.train_named_entity(window=1)
