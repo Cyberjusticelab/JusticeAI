c7852bb5abc1b9229971d248c87d638914e05532
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-11-02 04:00:59 -0400

    [#112] remove bootstrap due to node 8 compatibility; config travis to recognize issue number when revert commit exists; config docker to install node8 only (currently node9)

--- src/web_client/src/main.js
+++ src/web_client/src/main.js
@@ -4,18 +4,14 @@ import router from './router'
 import VueResource from 'vue-resource'
 import VueLocalStorage from 'vue-localstorage'
 import VueChatScroll from 'vue-chat-scroll'
-import BootstrapVue from 'bootstrap-vue'
 import VueUpload from 'vue-upload-component'
 import Icon from 'vue-awesome/components/Icon'
 import 'vue-awesome/icons'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
 Vue.use(VueLocalStorage)
 Vue.use(VueChatScroll)
-Vue.use(BootstrapVue)
 Vue.component('icon', Icon)
 Vue.component('file-upload', VueUpload)
 
