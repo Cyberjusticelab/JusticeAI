40d77726b5abd657be49287223b8adb81f9b5ecb
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-11-02 04:29:34 -0400

    [#112] implement element UI, improve landing page

--- src/web_client/src/main.js
+++ src/web_client/src/main.js
@@ -5,13 +5,16 @@ import VueResource from 'vue-resource'
 import VueLocalStorage from 'vue-localstorage'
 import VueChatScroll from 'vue-chat-scroll'
 import VueUpload from 'vue-upload-component'
+import ElementUI from 'element-ui'
 import Icon from 'vue-awesome/components/Icon'
 import 'vue-awesome/icons'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
 Vue.use(VueLocalStorage)
 Vue.use(VueChatScroll)
+Vue.use(ElementUI)
 Vue.component('icon', Icon)
 Vue.component('file-upload', VueUpload)
 
