f32c6ad26b326f4e7f159223abdde5c877d310e3
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-11-02 22:14:14 -0400

    [#112] pass usertype to rest call

--- src/web_client/src/components/Chat.vue
+++ src/web_client/src/components/Chat.vue
@@ -96,7 +96,7 @@
     <!-- Input Widow -->
     <div id="chat-input">
       <el-form>
-        <el-input id="chat-input-text" v-model="user.input" placeholder="Enter your message" autocomplete="off" :disabled="user.disableInput"></el-input>
+        <el-input id="chat-input-text" v-model="user.input" placeholder="Enter your message" autoComplete="off" :disabled="user.disableInput"></el-input>
         <el-button id="chat-input-submit" type="warning" :disabled="!user.input" @click="sendUserMessage()">SEND</el-button>
         <div id="chat-history-button" v-on:click="user.openChatHistory = !user.openChatHistory; getChatHistory()">
           <img v-if="!user.openChatHistory" alt="" src="../assets/history_open.png">
@@ -143,7 +143,8 @@ export default {
   methods: {
     initChatSession () {
       this.$http.post(this.api_url + 'new',{
-        name: this.$localStorage.get('username')
+        name: this.$localStorage.get('username'),
+        person_type: this.$localStorage.get('usertype')
       }).then(
         response => {
           this.$localStorage.set('zeusId', response.body.conversation_id)
