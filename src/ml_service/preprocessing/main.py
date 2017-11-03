ded58778bea6eb9a39453c39d60eb2900dbe631f
samuel-campbell <samuel.pcampbell@gmail.com>
2017-11-02 23:11:15 -0400

    [#105] removed extra instance of gensim

--- src/ml_service/preprocessing/main.py
+++ src/ml_service/preprocessing/main.py
@@ -12,11 +12,9 @@ if __name__ == '__main__':
     parser = Parser()
     j = 0
     for i in os.listdir(Global.Precedence_Directory):
-        if 'AZ-51205' in i and j < 5:
+        if 'AZ-51210' in i and j < 5:
             j += 1
             model = parser.parse(i)
             t = model.topics
             for j in range(len(t)):
-                print(model.core_topic[j])
-                print(model.topics[j])
-                print()
+                print(model.training_output())
