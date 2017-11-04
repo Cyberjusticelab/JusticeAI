57c779c08f8d58ca321afc22793dd452b21329e4
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-11-04 00:35:59 -0400

    [#112] fix webpack2 loader issue in docker build

--- src/web_client/build/webpack.base.conf.js
+++ src/web_client/build/webpack.base.conf.js
@@ -70,18 +70,14 @@ module.exports = {
         }
       },
       {
-        test: /\.s[a|c]ss$/,
-        loader: 'style!css!sass'
-      },
-      {
-        test: /\.s[a|c]ss$/,
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        test: /\.s[a|c]ss$/,
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        test: /\.s[a|c]ss$/,
-        loader: "sass-loader" // compiles Sass to CSS
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+          }, {
+            loader: "css-loader" // translates CSS into CommonJS
+          }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
       }
     ]
   }
