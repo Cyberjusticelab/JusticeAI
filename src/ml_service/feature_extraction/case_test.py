9d89ecf0839be3e2d51c87454f11e04fb7a7d893
samuel-campbell <samuel.pcampbell@gmail.com>
2017-11-06 11:23:25 -0500

    [#105] fixed relative import

--- src/ml_service/feature_extraction/case_test.py
+++ src/ml_service/feature_extraction/case_test.py
@@ -1,22 +1,22 @@
 from src.ml_service.feature_extraction import case
 import os
 
-case = case.Case(os.getcwd() + "/feature_extraction/dummy_cases/sample_case1.txt")
+c = case.Case(os.getcwd() + "/feature_extraction/dummy_cases/sample_case1.txt")
 
 
 def test_no_demande():
-    assert len(case.data["applicationID"]) == 1
-    assert case.data["applicationID"][0] == "1234567"
+    assert len(c.data["applicationID"]) == 1
+    assert c.data["applicationID"][0] == "1234567"
 
 
 def test_no_dossier():
-    assert len(case.data["fileID"]) == 1
-    assert case.data["fileID"][0] == "123456 12 12345678 D"
+    assert len(c.data["fileID"]) == 1
+    assert c.data["fileID"][0] == "123456 12 12345678 D"
 
 
 def test_total_hearings():
-    assert case.data["totalHearings"] == 2
+    assert c.data["totalHearings"] == 2
 
 
 def test_is_rectified():
-    assert case.data["isRectified"]
+    assert c.data["isRectified"]
