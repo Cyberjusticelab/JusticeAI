51baf88b786b3b8c78400ccef7e5674bb52bcc7f
Taimoor Rana <t-tarana@microsoft.com>
2017-11-07 17:20:43 -0500

    [#105] removed print statements and fixed linting issues

--- src/ml_service/feature_extraction/Clustering/k_means/fact_cluster.py
+++ src/ml_service/feature_extraction/Clustering/k_means/fact_cluster.py
@@ -1,6 +1,7 @@
 import re
 import nltk
 import os
+import numpy
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -9,7 +10,6 @@ from src.ml_service.feature_extraction.Preprocessing.Taimoor_Parser.fact_extract
 from src.ml_service.feature_extraction.Preprocessing.Taimoor_Parser.preprocessing import preprocessing
 from src.ml_service.GlobalVariables.GlobalVariable import Global
 from src.ml_service.feature_extraction.Preprocessing.Sam_Parser.PrecedenceParse import FrenchVectors
-import numpy
 
 
 class KMeansWrapper:
@@ -21,7 +21,7 @@ class KMeansWrapper:
         """
         raw_claim_text = extract_data_from_cases(precedent_directory, total_file_to_process)
         self.claim_text = preprocessing(raw_claim_text)
-        self.tfidf_matrix = self.init_tfidf()
+        self.data_matrix = self.create_data_matrix()
         self.cluster_size = cluster_size
         self.km = self.cluster()
         self.print("unlabeled_clusters")
@@ -44,7 +44,7 @@ class KMeansWrapper:
                 filtered_tokens.append(token)
         return filtered_tokens
 
-    def init_tfidf(self):
+    def create_data_matrix(self):
         # define vectorizer parameters
         tfidf_vectorizer = TfidfVectorizer(encoding="iso-8859-1",
                                            stop_words=stopwords.words('french'),
@@ -54,36 +54,24 @@ class KMeansWrapper:
                                            min_df=0.0, max_df=0.8, norm='l2',
                                            )
 
-        tf_idf_matrix = tfidf_vectorizer.fit_transform(self.claim_text)
-        print(tf_idf_matrix)
-        idf = tfidf_vectorizer.idf_
-        word_idf_dict = dict(zip(tfidf_vectorizer.get_feature_names(), idf))
+        tfidf_vectorizer.fit_transform(self.claim_text)
+        word_idf_dict = dict(zip(tfidf_vectorizer.get_feature_names(), tfidf_vectorizer.idf_))
         FrenchVectors.word_idf_dict = word_idf_dict
         claim_vec_list = []
-        print(len(word_idf_dict))
-        for claim in self.claim_text:
-            for word in claim:
-                try:
-                    FrenchVectors.word_vectors[word] = numpy.multiply(FrenchVectors.word_vectors[word],
-                                                                      word_idf_dict[word])
-                except TypeError:
-                    pass
-                except KeyError:
-                    pass
         for claim in self.claim_text:
             claim_vec_list.append(FrenchVectors.vectorize_sent(self.tokenize_only(claim)))
         return numpy.array(claim_vec_list)
 
     def cluster(self):
         km = KMeans(n_clusters=self.cluster_size, init='k-means++')
-        km.fit(self.tfidf_matrix)
+        km.fit(self.data_matrix)
         return km
 
     def print(self, file_name):
         f = open(file_name + ".txt", 'w')
         clusters = self.km.labels_.tolist()
         claim_cluster = [[] for i in range(self.cluster_size)]
-        print("cluster size: ",len(clusters))
+        print("cluster size: ", len(clusters))
         print("claim_text size: ", len(self.claim_text))
         for i in range(len(clusters)):
             claim_cluster[clusters[i]].append(self.claim_text[i])
@@ -93,6 +81,7 @@ class KMeansWrapper:
                 f.write(claim)
                 f.write("\n")
             f.write("\n\n========================================================================\n\n")
+
 
 if __name__ == '__main__':
     KMeansWrapper(os.path.normpath(Global.Precedence_Directory), 100, 10)
