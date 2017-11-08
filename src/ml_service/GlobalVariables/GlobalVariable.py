afefbc7dd9b0d1bcadf8ae77a439bd7d85de48a2
taimoorrana <taimoorrana1@gmail.com>
2017-11-08 00:14:36 -0500

    [#105] precedent directory path is now relative

--- src/ml_service/GlobalVariables/GlobalVariable.py
+++ src/ml_service/GlobalVariables/GlobalVariable.py
@@ -4,7 +4,11 @@
 # Change these variables to make code work on your machine
 # tmp solution since we can't download models online via
 # HTTP
+import os
 
 
 class Global:
-    Precedence_Directory = r"/Users/taimoorrana/Downloads/text_bk/"
+
+    __script_dir = os.path.abspath(__file__ + "/../../")
+    __rel_path = r'precedents/text_bk/'
+    Precedence_Directory = os.path.join(__script_dir, __rel_path)
