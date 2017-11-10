15ddd270afa17895931917398704d23e51220c76
choitwao <rkevincai@hotmail.com>
2017-11-09 20:34:45 -0500

    [#112] resolve dependencies error

--- src/web_client/test/unit/specs/Chat.spec.js
+++ src/web_client/test/unit/specs/Chat.spec.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import Chat from '@/components/Chat'
+import ElementUI from 'element-ui'
+import VueResource from 'vue-resource'
+import VueLocalStorage from 'vue-localstorage'
+Vue.use(ElementUI)
+Vue.use(VueLocalStorage)
+Vue.use(VueResource)
 
 describe('Chat.vue', () => {
 
