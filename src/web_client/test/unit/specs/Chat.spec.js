7d90bc8c89d62d75250eadc07a07ab6e10db36b6
choitwao <rkevincai@hotmail.com>
2017-11-09 22:15:26 -0500

    [#112] create basic unit test template with sinon js

--- src/web_client/test/unit/specs/Chat.spec.js
+++ src/web_client/test/unit/specs/Chat.spec.js
@@ -1,17 +1,41 @@
+/*
+import dependencies
+*/
+
 import Vue from 'vue'
 import Chat from '@/components/Chat'
 import ElementUI from 'element-ui'
 import VueResource from 'vue-resource'
 import VueLocalStorage from 'vue-localstorage'
+
+/*
+inject dependencies
+*/
+
 Vue.use(ElementUI)
 Vue.use(VueLocalStorage)
 Vue.use(VueResource)
 
+/*
+test
+*/
+
 describe('Chat.vue', () => {
 
-    it('tests something', () => {
-        const Ctor = Vue.extend(Chat)
-        const vm = new Ctor().$mount()
+    it('should resume chat session', () => {
+    	Vue.localStorage.set('zeusId', 1)
+    	const spy = sinon.spy(Chat.methods, "getChatHistory")
+        const vm = new Vue(Chat).$mount()
+        expect(spy.called).to.be.true
+        Chat.methods.getChatHistory.restore()
+        Vue.localStorage.remove('zeusId')
+    })
+
+    it('should init new chat session', () => {
+    	const spy = sinon.spy(Chat.methods, "getChatHistory")
+        const vm = new Vue(Chat).$mount()
+        expect(spy.called).to.be.false
+        Chat.methods.getChatHistory.reset()
     })
 
 })
