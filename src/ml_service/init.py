4125b7533b06bb4b865edea5cb0bc80894c9c691
samuel-campbell <samuel.pcampbell@gmail.com>
2017-11-11 15:22:48 -0500

    [#105] added new model

--- src/ml_service/init.py
+++ src/ml_service/init.py
@@ -1,7 +1,7 @@
 import nltk
-import urllib
 import os
 import time
+import requests
 from threading import Thread
 from sys import stdout
 
@@ -17,7 +17,8 @@ def monitorDownload(filename, filesize):
         current_size = os.stat(filename).st_size
         percentage = (current_size / filesize * 1.00) * 100
         percentage = float("{0:.2f}".format(percentage, 2))
-        stdout.write("\r%s - Download percentage: %f%%" % (filename, percentage))
+        stdout.write("\r%s - Download percentage: %.2f%%" %
+                     (filename, percentage))
         stdout.flush()
         time.sleep(3)
     print("\n[END] Downloading Binary for Word2Vec Model")
@@ -30,9 +31,18 @@ abs_file_path = os.path.join(script_dir, rel_path)
 
 if not os.path.exists(abs_file_path):
     print("[START] Downloading Binary for Word2Vec Model")
-    thr = Thread(target=monitorDownload, args=(abs_file_path, 312726847,))
+    thr = Thread(target=monitorDownload, args=(abs_file_path, 126052447,))
     thr.start()
-    urllib.request.urlretrieve(
-        "http://embeddings.org/frWac_non_lem_no_postag_no_phrase_500_skip_cut100.bin", abs_file_path)
+
+    response = requests.get('https://capstone.cyberjustice.ca/data/frWac_non_lem_no_postag_no_phrase_200_skip_cut100.bin',
+                            auth=(
+                                os.environ['CJL_USER'],
+                                os.environ['CJL_PASS']),
+                            stream=True,
+                            verify=False)
+    with open(abs_file_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
 else:
     print("Word Vector binary file requirement already satisfied.")
