87f719b83a64c693e2833d6043283a212b70b68a
Lance Lafontaine <lancelafontaine@protonmail.com>
2017-11-12 17:15:31 -0500

    [#75] Creates all initialization required for Flask database migrations

--- src/db_state_service/app.py
+++ src/db_state_service/app.py
@@ -1,5 +1,8 @@
 # app, db, and models originate from here
-import models
+import util
+util.load_src_dir_to_sys_path()
+
+from shared_modules.models import *
 
 from flask_migrate import Migrate
 migrate = Migrate(app, db)
