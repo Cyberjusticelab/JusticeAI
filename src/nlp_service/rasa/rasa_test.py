715d9440cfe4659c14db6280e7cce746edcae990
vynny <sylvain.czyzewski@gmail.com>
2017-11-12 17:36:34 -0500

    [#75] Improved tests -Improve RasaClassifier test to only load data once for entire test -Add Response test -Fixes backend service tests

--- src/nlp_service/rasa/rasa_test.py
+++ src/nlp_service/rasa/rasa_test.py
@@ -5,21 +5,33 @@ from rasa.rasa_classifier import RasaClassifier
 
 
 class TestRasaClassifier(unittest.TestCase):
-    def setUp(self):
-        self.rasaClassifier = RasaClassifier()
-        self.rasaClassifier.train(force_train=True)
+    rasaClassifier = None
 
-    def test_rasaclassifier_should_instantiate(self):
+    @classmethod
+    def setUpClass(cls):
+        cls.rasaClassifier = RasaClassifier()
+        cls.rasaClassifier.train(force_train=True)
+
+    def test_instantiate(self):
         self.assertIsNotNone(self.rasaClassifier)
 
-    def test_rasaclassifier_should_classify_claimcategory(self):
+    def test_classify_claimcategory(self):
         classifier_output = self.rasaClassifier.classify_problem_category("I am being kicked out.")
         self.assertIsNotNone(classifier_output)
 
-    def test_rasaclassifier_should_extract_factentities(self):
+    def test_extract_factentities(self):
         classifier_output = self.rasaClassifier.classify_fact("is_student", "I am a student.")
         self.assertIsNotNone(classifier_output)
 
-    def test_rasaclassifier_shouldnt_extract_nonexistent_factentities(self):
+    def test_nonexistent_factentities(self):
         classifier_output = self.rasaClassifier.classify_fact("does_not_exist", "I am a student.")
         self.assertIsNone(classifier_output)
+
+    def test_percent_difference(self):
+        intent_dict = {
+            'intent_ranking': [
+                {'confidence': 0.5},
+                {'confidence': 0.5}
+            ]
+        }
+        self.assertEqual(RasaClassifier.intent_percent_difference(intent_dict), 0)
--- src/nlp_service/services/responseStrings.py
+++ src/nlp_service/services/responseStrings.py
@@ -2,24 +2,6 @@ import random
 
 
 class Responses:
-    # Asking for Acceptance to legal conditions
-    disclaimer = [
-        "Hello {name}! Before we start, I want to make it clear that I am not a replacement for a lawyer and any information I provide you with is not meant to be construed as legal advice. Always check in with your legal professional. You can read more about our terms of use <a href='/legal' target='_blank'>here</a>. Do you accept these conditions?"
-    ]
-
-    # Asking for initial problem description
-    problem_inquiry_landlord = [
-        "So {name}, I see you're a landlord. What issue can I help you with today?",
-        "What kind of issue are you having as a landlord, {name}?",
-        "I can help you with all sorts of landlord issues, {name}! Describe your problem to me, let's see what we can do."
-    ]
-
-    problem_inquiry_tenant = [
-        "I see you're a tenant, {name}. If you have it on hand, it would be very helpful if you could upload your lease. What issue can I help you with today?",
-        "What kind of issue are you having as a tenant, {name}? Upload your lease if you have it, it might help in resolving your issues.",
-        "I can help you with all sorts of tenant issues, {name}! Describe your problem to me and upload your lease if available."
-    ]
-
     # Acknowledge claim category resolution
     category_acknowledge = [
         "I see, you're having issues with {claim_category}. {first_question}",
