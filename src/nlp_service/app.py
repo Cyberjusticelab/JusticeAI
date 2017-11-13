9a14d8c5bec8a5fdb3b953b944b6bdd838ea3d02
mihaiqc <mihai_qc@hotmail.fr>
2017-11-12 19:48:33 -0500

    [#75] Documentation - Documenting app.py as per Pull Request Review comments

--- src/nlp_service/app.py
+++ src/nlp_service/app.py
@@ -15,6 +15,12 @@ app = Flask(__name__)
 # DB Setup
 db = database.connect(app, 'postgres', os.environ['POSTGRES_PASSWORD'], 'postgres')
 
+"""
+  These functions establishes HTTP routes for the core functionality of the src/nlp_service/controllers/nlpController.py
+  The core functionality of classify_claim_category and classify_fact_value are explained further in the file mentioned above
+  
+"""
+
 
 @app.route("/claim_category", methods=['POST'])
 def classify_claim_category():
