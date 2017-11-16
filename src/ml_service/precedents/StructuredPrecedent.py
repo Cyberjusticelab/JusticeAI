8485d13b2dca2312697ba1e6e9a82ce556d1460b
taimoorrana <taimoorrana1@gmail.com>
2017-11-15 22:06:00 -0500

    updated code structure due to change in requirements

--- src/ml_service/precedents/StructuredPrecedent.py
+++ src/ml_service/precedents/StructuredPrecedent.py
@@ -4,36 +4,34 @@ from src.ml_service.global_variables.global_variable import InformationType
 class StructuredPrecedent:
 
     FACT_CATEGORY = 0
-    FACT_VECTOR = 0
-    FACT_TUPLE = 1
+    FACTS = "facts"
+    OUTCOMES = "outcomes"
+    LEASE_TERMINATED = "lease_terminated"
 
-    def __init__(self, fact_labels, data_tuple):
+    def __init__(self, fact_labels, fact_data_tuple, outcome_labels, outcome_data_tuple):
         """
         :param fact_labels [int]: fact labels/categories for each sentence in the data_tuple
         :param data_tuple <[int], [string], [string]>: vectors, transformed sentences, original sentence
         """
-        self.fact_labels = fact_labels
-        self.data_tuple = data_tuple
+        self.precedents = {}
+        self.create_vector(fact_labels, fact_data_tuple, self.FACTS)
+        self.create_vector(outcome_labels, outcome_data_tuple, self.OUTCOMES)
 
-    def get_precedents(self):
+    def create_vector(self, labels, data_tuple, data_type):
         """
         Recreates precedents from fact clusters
         :return [[int or None],(int,string)]: Structured precedents where the first element represents the fact vector
                                               The second element contains all the facts, with their category number
         """
-        precedents = {}
-        unique_labels = set(self.fact_labels)
-        for label in unique_labels:
-            for raw_fact_tuple, file_name_tuple in zip(enumerate(self.data_tuple[InformationType.FACTS.value][self.fact_labels == label]),
-                                                       enumerate(self.data_tuple[InformationType.PRECEDENTS_FILE_NAMES.value][self.fact_labels == label])):
+        unique_labels = set(labels)
+        for label in (temp_label for temp_label in unique_labels if temp_label >= 0):
+            for raw_fact_tuple, file_name_tuple in zip(enumerate(data_tuple[InformationType.FACTS.value][labels == label]),
+                                                       enumerate(data_tuple[InformationType.PRECEDENTS_FILE_NAMES.value][labels == label])):
 
                 file_name = file_name_tuple[1].replace(".txt", "")
-                if file_name not in precedents:
+                if file_name not in self.precedents:
                     # Create new entry with file name
-                    precedents[file_name] = [[None]*len(unique_labels), [raw_fact_tuple]]
-                else:
-                    # add fact to existing entry
-                    precedents[file_name][self.FACT_TUPLE].append(raw_fact_tuple)
+                    self.precedents[file_name] = dict.fromkeys([self.FACTS, self.OUTCOMES, self.LEASE_TERMINATED])
+                    self.precedents[file_name][data_type] = [0] * len(unique_labels)
 
-                precedents[file_name][self.FACT_VECTOR][raw_fact_tuple[self.FACT_CATEGORY]] = 1  # 1 signifies that the fact exists
-        return precedents
+                self.precedents[file_name][data_type][raw_fact_tuple[self.FACT_CATEGORY]] = 1  # 1 signifies that the fact exists
