cdcdc5ed2adb7d727e6189a894a452b2d4af988c
vynny <sylvain.czyzewski@gmail.com>
2017-11-18 15:23:59 -0500

    [#153] Adds regex for demands

--- src/ml_service/feature_extraction/regex/regex_lib.py
+++ src/ml_service/feature_extraction/regex/regex_lib.py
@@ -5,3 +5,15 @@ class RegexLib:
     regex_list =[
 
     ]
+
+    regex_demands = {
+        "tenant_eviction": re.compile(r"(locateur|locatrice).+demande.+résiliation.+bail.+expulsion.+locataires"),
+        "landlord_lease_termination": re.compile(r"(demande.+)?(locateur|locatrice).+résiliation.+bail"),
+        "tenant_cover_rent": re.compile(r"(demande.+)?locateur(.+demande)?.+recouvrement\s(du|de)\sloyer"),
+        "paid_judicial_fees": re.compile(r"(locateur|locatrice)\sdemande.+(frais\sjudiciaires)"),
+        "landlord_money_cover_rent": re.compile(r"(locateur|locatrice)\sdemande.+recouvrement\s(du|de)\sloyer")
+    }
+
+    regex_facts = {
+
+    }
