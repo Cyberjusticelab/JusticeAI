b15634006d11d8f5a64464925af1dfef5759724f
vynny <sylvain.czyzewski@gmail.com>
2017-11-18 16:05:18 -0500

    [#153] Adds regex for facts

--- src/ml_service/feature_extraction/regex/regex_lib.py
+++ src/ml_service/feature_extraction/regex/regex_lib.py
@@ -15,5 +15,18 @@ class RegexLib:
     }
 
     regex_facts = {
-
+        "tenant_lease_indeterminate": re.compile(r"bail.+(durée\sindéterminée)"),
+        "tenant_lease_fixed": re.compile(r"bail\s(reconduit\s)?(du|se\sterminant).+au(.+\d{4})"),
+        "tenant_monthly_payment": re.compile(r"loyer\smensuel\sde([\d\s]+)(\$|\s\$)"),
+        "tenant_owes_rent": re.compile(r"(locataire(s)?\sdoivent([\d\s]+)(\$|\s\$)|locateur.+créance.+([\d\s]+)(\$|\s\$).+loyers\simpayés)"),
+        "tenant_rent_not_paid_more_3_weeks": re.compile(r"locataire(s)?.+retard.+plus.+trois\ssemaines.+(paiement\sdu\sloyer)"),
+        "tenant_violence": re.compile(r"comportement\sviolent"),
+        "tenant_request_cancel_lease": re.compile(r""),
+        "tenant_not_request_cancel_lease": re.compile(r"jamais.+(résiliation\sde\sbail)"),
+        "tenant_pay_before_judgment": re.compile(r""),
+        "tenant_group_responsability": re.compile(r"bail.+(prévoit\spas).+locataire(s)?.+(solidairement\sresponsables).+locateur"),
+        "tenant_individual_responsability": re.compile(r""),
+        "tenant_withold_rent_without_permission": re.compile(r"locataire(s)?.+(ne\speut).+(faire\sjustice).+retenir.+loyer.+(sans.+Tribunal)?"),
+        "landlord_prejudice_justified": re.compile(r"préjudice.+(causé)?.+(locateur|locatrice).+justifie.+décision"),
+        "landlord_not_prejudice_justified": re.compile(r"")
     }
