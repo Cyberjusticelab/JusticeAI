2a728fd8260bdbef6b7617794741d670fc32dd17
taimoorrana <taimoorrana1@gmail.com>
2017-11-20 15:36:42 -0500

    [#183] updated bin dump path

--- src/ml_service/regex_demand.py
+++ src/ml_service/regex_demand.py
@@ -128,7 +128,7 @@ fill_dict(precedent_vector, tenant_negligence, 'tenant_negligence')
 fill_dict(precedent_vector, tenant_is_bothered, 'tenant_is_bothered')
 fill_dict(precedent_vector, apartment_impropre, 'apartment_impropre')
 
-joblib.dump(precedent_vector, Load.script_dir + 'precedent_fact_vector_from_regexes.bin')
+joblib.dump(precedent_vector, Load.script_dir + 'ml_models/precedent_fact_vector_from_regexes.bin')
 
 print("diff total:")
 print(len(inter))
