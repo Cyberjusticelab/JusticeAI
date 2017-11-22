6b1be440fa631eb42dbd80c654c132e01b25e9ec
samuel-campbell <samuel.pcampbell@gmail.com>
2017-11-22 14:59:26 -0500

    [#183] util unittests

--- src/ml_service/init_test.py
+++ src/ml_service/init_test.py
@@ -1,9 +1,8 @@
 import os
 import unittest
-
-from util.constant import Global
-
+from util.constant import Path
 
 class TestStringMethods(unittest.TestCase):
     def test_init(self):
-        self.assertTrue(os.path.isfile(Global.word_vector_directory + 'non-lem.bin'))
+        import init
+        self.assertTrue(os.path.isfile(Path.binary_directory + 'non-lem.bin'))
