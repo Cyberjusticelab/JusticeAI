a4716579408beaa3a6dc8d33b9025b548df35c4c
vynny <sylvain.czyzewski@gmail.com>
2017-11-23 18:57:10 -0500

    [#183] Changes to ml service -Added new facts to ml service -Adds method to submit fact list to ml service, unimplemented

--- src/nlp_service/services/mlService.py
+++ src/nlp_service/services/mlService.py
@@ -39,24 +39,71 @@ def submit_resolved_fact(conversation, current_fact, entity_value):
     }
 
 
+"""
+Submits list of resolved facts to ML endpoint. Only done once all
+facts have been asked and resolved.
+Returns the first fact to ask a question for, based on claim category.
+conversation: the current conversation
+:returns Outcomes vector with true/false, depending on chance of winning case
+"""
+
+
+def submit_resolved_fact_list(conversation):
+    pass
+
+
+"""
+Dummy method that returns next fact given resolved facts. Stub implementation for
+proposed ML service.
+claim_category: claim category of the conversation
+facts_resolved: list of resolved fact keys
+:returns Next fact id to ask
+"""
+
+
 def dummy_next_fact(claim_category, facts_resolved):
+    all_facts = [
+        "apartment_impropre",
+        "apartment_infestation",
+        "bothers_others",
+        "disrespect_previous_judgement",
+        "landlord_inspector_fees",
+        "landlord_notifies_tenant_retake_apartment",
+        "landlord_relocation_indemnity_fees",
+        "landlord_rent_change",
+        "landlord_rent_change_doc_renseignements",
+        "landlord_retakes_apartment",
+        "landlord_retakes_apartment_indemnity",
+        "landlord_sends_demand_regie_logement",
+        "proof_of_late",
+        "proof_of_revenu",
+        "rent_increased",
+        "tenant_bad_payment_habits",
+        "tenant_continuous_late_payment",
+        "tenant_damaged_rental",
+        "tenant_dead",
+        "tenant_declare_insalubre",
+        "tenant_financial_problem",
+        "tenant_group_responsability",
+        "tenant_individual_responsability",
+        "tenant_landlord_agreement",
+        "tenant_lease_fixed",
+        "tenant_left_without_paying",
+        "tenant_monthly_payment",
+        "tenant_negligence",
+        "tenant_not_request_cancel_lease",
+        "tenant_owes_rent",
+        "tenant_refuses_retake_apartment",
+        "tenant_rent_not_paid_more_3_weeks",
+        "tenant_violence",
+        "tenant_withold_rent_without_permission"
+    ]
+
     fact_dict = {
-        "lease_termination": [
-            "lease_type",
-            "has_lease_expired",
-            "is_student",
-            "is_habitable"
-        ],
-        "rent_change": [
-            "lease_type",
-            "has_lease_expired"
-        ],
-        "nonpayment": [
-            "has_lease_expired"
-        ],
-        "deposits": [
-            "lease_type"
-        ]
+        "lease_termination": all_facts,
+        "rent_change": all_facts,
+        "nonpayment": all_facts,
+        "deposits": all_facts
     }
 
     all_category_facts = fact_dict[claim_category.value.lower()]
