e07ea586a54cd820614d5f8f0a05f25dbd1998cf
arekmano <manoukian.arek@gmail.com>
2017-11-23 09:37:21 -0500

    [#183] Added documentation to the controller

--- src/ml_service/web/ml_controller.py
+++ src/ml_service/web/ml_controller.py
@@ -10,6 +10,32 @@ indices = TagPrecedents().get_intent_indice()
 
 
 def predict_outcome(input_json):
+    """
+        Makes a prediction based on the input json
+        input_json: Dict containing the facts and demands
+                    The input json must be as follows:
+                    {
+                        "demands" : {
+                            "demand1": 1 or 0,
+                            "demand2": 1 or 0,
+                            "demand3": 1 or 0,
+                            etc
+                        },
+                        "facts" : {
+                            "fact1": 1 or 0,
+                            "fact2": 1 or 0,
+                            "fact3": 1 or 0,
+                            etc
+                        }
+                    }
+                    It is not necessary to include ALL demands or facts,
+                    some may be omitted
+        returns: a dict containing all the predictions
+                currently, its format is as follows:
+                {
+                    "lease_resiliation" : 1 or 0
+                }
+    """
     demands_vector = __dict_to_vector(input_json['demands'], 'demands_vector')
     facts_vector = __dict_to_vector(input_json['facts'], 'facts_vector')
     outcome_vector = linear_classifier.predict(facts_vector)
@@ -17,6 +43,20 @@ def predict_outcome(input_json):
 
 
 def __dict_to_vector(input_dict, input_type):
+    """
+        Converts a dictionary to vector form, readable by ML
+        input_dict: dictionary containing all facts or demands
+                    It is as follows:
+                    {
+                        "demand1": 1 or 0,
+                        "demand2": 1 or 0,
+                        "demand3": 1 or 0,
+                        etc
+                    }
+        input_type: either 'facts_vector' or 'demands_vector', depending
+                    on which is being generated
+        returns: a vector of 1s and 0s
+    """
     output_vector = np.zeros(len(indices[input_type]))
     for index, val in indices[input_type]:
         if not val in input_dict:
