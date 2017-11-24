c95c5ef407d3dafba06190f150270e253d36d515
vynny <sylvain.czyzewski@gmail.com>
2017-11-23 21:03:02 -0500

    [#183] Added new tests for Responses service

--- src/nlp_service/services/responseStrings_test.py
+++ src/nlp_service/services/responseStrings_test.py
@@ -14,3 +14,15 @@ class TestResponse(unittest.TestCase):
     def test_responses_service_empty(self):
         question = Responses.fact_question("does_not_exist")
         self.assertTrue(question in self.responseInstance.fact_questions["missing_response"])
+
+    def test_responses_prediction(self):
+        question = Responses.prediction_statement("LEASE_TERMINATION", True)
+        self.assertTrue(question in self.responseInstance.prediction["LEASE_TERMINATION"]['success'])
+
+    def test_responses_prediction_fail(self):
+        question = Responses.prediction_statement("LEASE_TERMINATION", False)
+        self.assertTrue(question in self.responseInstance.prediction["LEASE_TERMINATION"]['fail'])
+
+    def test_responses_prediction_empty(self):
+        question = Responses.prediction_statement("does_not_exist", True)
+        self.assertTrue(question in self.responseInstance.prediction["missing_category"])
