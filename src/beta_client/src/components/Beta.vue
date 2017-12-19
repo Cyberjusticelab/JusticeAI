d163b14beda6ba611c88ac7f4c88597a811a9115
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-12-19 15:01:53 -0500

    [#218] create animation

--- src/beta_client/src/components/Beta.vue
+++ src/beta_client/src/components/Beta.vue
@@ -6,37 +6,45 @@
     <el-container id="beta-component">
         <div id="beta-page">
             <div id="beta-page-nav">
-                <p v-on:click="chat=!chat">ABOUT US</p> <!--todo-->
+                <p>ABOUT US</p>
             </div>
-            <div id="beta-page-title" v-bind:class="{ 'beta-page-title-in-chat': chat}">
-                <el-row>
-                    <el-col :md="24">
-                        <div id="title-image">
-                            <img alt="" src="../assets/beta_page_zeus_avatar.png" id="beta-page-zeus">
-                            <img alt="" src="../assets/beta_page_logo.png" id="beta-page-logo">
-                        </div>
-                    </el-col>
-                    <el-col :md="24">
-                        <div id="title-text">
-                            <h2 id="beta_page_description">A conflict prevention tool that uses artificial intelligence and case data to answer questions from landlords and tenants regarding the likely outcome of a dispute.</h2>
-                        </div>
-                    </el-col>
-                </el-row>
-            </div>
-            <div id="beta-page-sign-up" v-if="chat">
-                <div id="beta-page-sign-up-placeholder">
+            <transition name="fade">
+                <div id="beta-page-title" v-bind:class="{ 'beta-page-title-in-chat': chat}">
+                    <el-row>
+                        <el-col :md="24">
+                            <div id="title-image">
+                                <img alt="" src="../assets/beta_page_zeus_avatar.png" id="beta-page-zeus">
+                                <img alt="" src="../assets/beta_page_logo.png" id="beta-page-logo">
+                            </div>
+                        </el-col>
+                        <el-col :md="24">
+                            <div id="title-text">
+                                <h2 id="beta_page_description">A conflict prevention tool that uses artificial intelligence and case data to answer questions from landlords and tenants regarding the likely outcome of a dispute.</h2>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </div>
+            </transition>
+            <transition name="fade">
+                <div id="beta-page-sign-up" v-if="chat">
+                    <div id="beta-page-sign-up-nav" v-on:click="chat=!chat">
+                        <img alt="" src="../assets/close.png">
+                        <p>Back</p>
+                    </div>
                     <chat></chat>
                 </div>
-            </div>
-            <div id="beta-page-button-group" v-if="!chat">
-                <el-row>
-                    <el-col :md="24">
-                        <div id="button-group">
-                            <el-button type="warning" v-on:click="chat = !chat">SIGN UP TO BETA</el-button>
-                        </div>
-                    </el-col>
-                </el-row>
-            </div>
+            </transition>
+            <transition name="fade">
+                <div id="beta-page-button-group" v-if="!chat">
+                    <el-row>
+                        <el-col :md="24">
+                            <div id="button-group">
+                                <el-button type="warning" v-on:click="chat = !chat">SIGN UP TO BETA</el-button>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </div>
+            </transition>
             <div id="beta-page-footer">
                 <a href="http://www.cyberjustice.ca/" target="_blank">© 2017 Cyberjustice Laboratory</a>
             </div>
@@ -58,7 +66,7 @@ export default {
     },
     data () {
         return {
-            chat: true
+            chat: false
         }
     }
 }
