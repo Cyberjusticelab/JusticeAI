73cb04362c28ba20fe7835f49509b18f754edf37
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-12-19 13:32:38 -0500

    [#218] beta landing page responsive and description

--- src/beta_client/src/components/Beta.vue
+++ src/beta_client/src/components/Beta.vue
@@ -5,12 +5,20 @@
 <template>
     <el-container id="beta-component">
         <div id="beta-page">
+            <div id="about-us">
+                <p>ABOUT US</p>
+            </div>
             <div id="beta-page-title">
                 <el-row>
                     <el-col :md="24">
                         <div id="title-image">
                             <img alt="" src="../assets/beta_page_zeus_avatar.png" id="beta-page-zeus">
                             <img alt="" src="../assets/beta_page_logo.png" id="beta-page-logo">
+                        </div>
+                    </el-col>
+                    <el-col :md="24">
+                        <div id="title-text">
+                            <h2 id="beta_page_description">A conflict prevention tool that uses artificial intelligence and case data to answer questions from landlords and tenants regarding the likely outcome of a dispute.</h2>
                         </div>
                     </el-col>
                 </el-row>
@@ -23,6 +31,9 @@
                         </div>
                     </el-col>
                 </el-row>
+            </div>
+            <div id="beta-page-footer">
+                <a href="http://www.cyberjustice.ca/" target="_blank">© 2017 Cyberjustice Laboratory</a>
             </div>
         </div>
     </el-container>
