40bb57c27211cee30b29549aef8cf84c5e28ad21
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-12-19 18:10:43 -0500

    [#218] finalize about us page

--- src/beta_client/src/components/Beta.vue
+++ src/beta_client/src/components/Beta.vue
@@ -6,7 +6,7 @@
     <el-container id="beta-component">
         <div id="beta-page">
             <div id="beta-page-nav">
-                <p>ABOUT US</p>
+                <p v-on:click="aboutUs=!aboutUs" v-if="!aboutUs">ABOUT US</p>
             </div>
             <transition name="fade">
                 <div id="beta-page-title" v-bind:class="{ 'beta-page-title-in-chat': chat}">
@@ -49,11 +49,24 @@
                 <a href="http://www.cyberjustice.ca/" target="_blank">© 2017 Cyberjustice Laboratory</a>
             </div>
         </div>
-        <!-- about us section todo
-        <div id="beta-page-about-us">
-            <p>dsad</p>
+        <div id="beta-page-about-us" v-if="aboutUs">
+            <div id="beta-page-about-us-nav">
+                <p v-on:click="aboutUs=!aboutUs">CLOSE</p>
+            </div>
+            <h2>Cyberjustice Laboratory</h2>
+            <img alt="" src="../assets/cjl.jpg" id="beta-page-zeus">
+            <div class="about-us-text">
+                <p>The Cyberjustice Lab is a hub for thought and creativity, where justice processes are modelled and reimagined. More specifically, the Laboratory analyses the impact of technologies on justice and develops concrete technological tools that are adapted to the reality of justice systems.</p>
+                <p>Hosted at the Faculty of Law of the Université de Montréal, its multidisciplinary international research team extends to 45 researchers from 23 institutions in Europe, the Americas, China and Australia, whose investigative goals tend towards improving the allocation and administration of judicial services so as to make optimal use of advances in technology.</p>
+                <p>The Cyberjustice Laboratory is designing an artificial intelligence tool capable of assessing the validity of a litigant’s claim and proposing possible avenues for resolution.</p>
+                <p>The solution takes the shape of a dynamic conversational agent designed to predict the likely outcomes of a variety of landlord/tenant disputes, based on personalized interactions with the disputant and analysis of relevant case law using machine learning algorithms.</p>
+                <p>The project tests the boundaries of machine learning and natural language processing applied to the complex domain of law, and attempts to validate the utility of such a tool for stakeholders.</p>
+            </div>
+            <h2>Engineer Team</h2>
+            <div class="about-us-text">
+                <p>We are a team of software engineering students from Concordia University...</p>
+            </div>
         </div>
-        -->
     </el-container>
 </template>
 
@@ -66,7 +79,8 @@ export default {
     },
     data () {
         return {
-            chat: false
+            chat: false,
+            aboutUs: false
         }
     }
 }
