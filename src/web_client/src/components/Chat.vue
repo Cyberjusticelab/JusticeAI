c8c15bf800c1a686117f327bff5472ba523c1863
mihaiqc <mihai_qc@hotmail.fr>
2017-12-21 14:36:12 -0500

    [#161] Separation of chat-history in progress

--- src/web_client/src/components/Chat.vue
+++ src/web_client/src/components/Chat.vue
@@ -1,5 +1,5 @@
 <style lang="scss" scoped>
-@import "../theme/Chat"
+  @import "../theme/Chat"
 </style>
 
 <template>
@@ -7,13 +7,34 @@
     <!-- Chat History -->
     <transition name="fade">
       <div id="chat-history" v-if="user.openChatHistory" v-chat-scroll>
-        <ul>
-          <li v-for="conv in chatHistory">
-            <h3>{{ conv.sender_type }}</h3>
-            <p>- {{ conv.text }}</p>
-            <p>on {{ conv.timestamp.split('T')[0] }} at {{ conv.timestamp.split('T')[1].substring(0,8) }}</p>
-          </li>
-        </ul>
+        <el-row>
+          <el-col :sm="8">
+            <div>
+              <ul>
+                <li v-for="conv in chatHistory">
+                  <h3>{{ conv.sender_type }}</h3>
+                  <p>- {{ conv.text }}</p>
+                  <p>on {{ conv.timestamp.split('T')[0] }} at {{ conv.timestamp.split('T')[1].substring(0,8) }}</p>
+                </li>
+              </ul>
+            </div>
+          </el-col>
+          <el-col :sm="4">
+            <div id="previous-facts">
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+              <ul><li>FACT HERE</li></ul>
+            </div>
+          </el-col>
+        </el-row>
       </div>
     </transition>
     <!-- End of Chat History -->
@@ -54,7 +75,7 @@
                       <button v-on:click="confirmBotResponse(false)">No</button>
                     </div>
                     <div v-if="factType !== 'boolean'">
-                      <input type="text" v-model="userConfirmationText" />
+                      <input type="text" v-model="userConfirmationText"/>
                       <button v-on:click="confirmBotResponse(userConfirmationText)">OK</button>
                     </div>
                   </div>
@@ -75,12 +96,19 @@
                 </file-upload>
               </transition>
               <div id="file-upload-button-group" v-if="zeus.filePrompt">
-                <el-button v-show="!$refs.upload || !$refs.upload.active" @click.prevent="$refs.upload.active = true" type="warning"  :disabled="zeus.file.length == 0">Upload</el-button>
-                <el-button v-show="$refs.upload && $refs.upload.active" @click.prevent="$refs.upload.active = false" type="warning"  >Stop</el-button>
-                <p v-if="zeus.file[0] && zeus.file[0].success && $refs.upload.uploaded">Successfully uploaded <span>{{ zeus.file[0].name }}</span></p>
+                <el-button v-show="!$refs.upload || !$refs.upload.active" @click.prevent="$refs.upload.active = true"
+                           type="warning" :disabled="zeus.file.length == 0">Upload
+                </el-button>
+                <el-button v-show="$refs.upload && $refs.upload.active" @click.prevent="$refs.upload.active = false"
+                           type="warning">Stop
+                </el-button>
+                <p v-if="zeus.file[0] && zeus.file[0].success && $refs.upload.uploaded">Successfully uploaded <span>{{ zeus.file[0].name }}</span>
+                </p>
               </div>
               <div id="pre-selected-answer-group" v-if="zeus.suggestion && zeus.input">
-                <el-button v-for="answer in zeus.suggestion" :key="answer.id" type="warning"   v-on:click="user.input = answer; sendUserMessage()">{{ answer }}</el-button>
+                <el-button v-for="answer in zeus.suggestion" :key="answer.id" type="warning"
+                           v-on:click="user.input = answer; sendUserMessage()">{{ answer }}
+                </el-button>
               </div>
             </div>
           </el-col>
@@ -109,7 +137,8 @@
     <!-- Input Widow -->
     <div id="chat-input">
       <form v-on:submit.prevent="sendUserMessage()">
-        <el-input id="chat-input-text" autosize v-model="user.input" placeholder="Enter your message" autoComplete="off" :disabled="user.disableInput"></el-input>
+        <el-input id="chat-input-text" autosize v-model="user.input" placeholder="Enter your message" autoComplete="off"
+                  :disabled="user.disableInput"></el-input>
         <el-button id="chat-input-submit" type="warning" :disabled="!user.input" native-type="submit">SEND</el-button>
         <div id="chat-history-button" v-on:click="user.openChatHistory = !user.openChatHistory; getChatHistory()">
           <img v-if="!user.openChatHistory" alt="" src="../assets/history_open.png">
@@ -123,133 +152,133 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      api_url: process.env.API_URL,
-      uploadUrl: new String,
-      connectionError: false,
-      chatHistory: new Array,
-      numMessageSinceChatHistory: 0,
-      isLoggedIn: false, //TODO: account feature
-      factType: 'boolean',
-      zeus: {
-        input: null,
-        file: new Array,
-        filePrompt: false,
-        enableUserConfirmation: false,
-        suggestion: new Array
-      },
-      user: {
-        name: null,
-        input: null,
-        isSent: false,
-        openChatHistory: false,
-        disableInput: false
+  export default {
+    data() {
+      return {
+        api_url: process.env.API_URL,
+        uploadUrl: new String,
+        connectionError: false,
+        chatHistory: new Array,
+        numMessageSinceChatHistory: 0,
+        isLoggedIn: false, //TODO: account feature
+        factType: 'boolean',
+        zeus: {
+          input: null,
+          file: new Array,
+          filePrompt: false,
+          enableUserConfirmation: false,
+          suggestion: new Array
+        },
+        user: {
+          name: null,
+          input: null,
+          isSent: false,
+          openChatHistory: false,
+          disableInput: false
+        }
       }
-    }
-  },
-  created () {
-    if (this.$localStorage.get('zeusId')) {
-      this.getChatHistory()
-    } else {
-      this.initChatSession()
-    }
-  },
-  methods: {
-    initChatSession () {
-      this.$http.post(this.api_url + 'new',{
-        name: this.$localStorage.get('username'),
-        person_type: this.$localStorage.get('usertype')
-      }).then(
-        response => {
-          this.$localStorage.set('zeusId', response.body.conversation_id)
-          this.user.input = ''
-          this.sendUserMessage()
-          this.uploadUrl = this.api_url + 'conversation/' + response.body.conversation_id + '/files'
-        },
-        response => {
-          this.connectionError = true
-        }
-      )
     },
-    sendUserMessage () {
-      this.$http.post(this.api_url + 'conversation', {
-        conversation_id: this.$localStorage.get('zeusId'),
-        message: this.user.input
-      }).then(
-        response => {
-          this.zeus.input = null
-          this.user.isSent = this.user.input != ''
-          this.zeus.filePrompt = false;
-          this.numMessageSinceChatHistory += 1
-          setTimeout(() => {
-            this.configChat(response.body)
-          }, 1100)
-        },
-        response => {
-          this.connectionError = true
-        }
-      )
+    created() {
+      if (this.$localStorage.get('zeusId')) {
+        this.getChatHistory()
+      } else {
+        this.initChatSession()
+      }
     },
-    getChatHistory () {
-      let zeusId = this.$localStorage.get('zeusId')
-      this.$http.get(this.api_url + 'conversation/' + zeusId).then(
-        response => {
-          this.chatHistory = response.body.messages
-          this.numMessageSinceChatHistory = 0
-          this.user.name = response.body.name
-          if (!this.zeus.input) {
-            this.configChat(this.chatHistory[this.chatHistory.length-1])
+    methods: {
+      initChatSession() {
+        this.$http.post(this.api_url + 'new', {
+          name: this.$localStorage.get('username'),
+          person_type: this.$localStorage.get('usertype')
+        }).then(
+          response => {
+            this.$localStorage.set('zeusId', response.body.conversation_id)
+            this.user.input = ''
+            this.sendUserMessage()
+            this.uploadUrl = this.api_url + 'conversation/' + response.body.conversation_id + '/files'
+          },
+          response => {
+            this.connectionError = true
           }
-          this.uploadUrl = this.api_url + 'conversation/' + zeusId + '/files'
-          this.setEnableUserConfirmation()
-        },
-        response => {
-          this.connectionError = true
+        )
+      },
+      sendUserMessage() {
+        this.$http.post(this.api_url + 'conversation', {
+          conversation_id: this.$localStorage.get('zeusId'),
+          message: this.user.input
+        }).then(
+          response => {
+            this.zeus.input = null
+            this.user.isSent = this.user.input != ''
+            this.zeus.filePrompt = false;
+            this.numMessageSinceChatHistory += 1
+            setTimeout(() => {
+              this.configChat(response.body)
+            }, 1100)
+          },
+          response => {
+            this.connectionError = true
+          }
+        )
+      },
+      getChatHistory() {
+        let zeusId = this.$localStorage.get('zeusId')
+        this.$http.get(this.api_url + 'conversation/' + zeusId).then(
+          response => {
+            this.chatHistory = response.body.messages
+            this.numMessageSinceChatHistory = 0
+            this.user.name = response.body.name
+            if (!this.zeus.input) {
+              this.configChat(this.chatHistory[this.chatHistory.length - 1])
+            }
+            this.uploadUrl = this.api_url + 'conversation/' + zeusId + '/files'
+            this.setEnableUserConfirmation()
+          },
+          response => {
+            this.connectionError = true
+          }
+        )
+      },
+      configChat(conversation) {
+        this.zeus.input = conversation.message || conversation.html || conversation.text
+        this.zeus.filePrompt = (conversation.file_request !== undefined) && (conversation.file_request !== null)
+        if (conversation.possible_answers == undefined) {
+          this.zeus.suggestion = []
+        } else {
+          this.zeus.suggestion = JSON.parse(conversation.possible_answers) || []
         }
-      )
-    },
-    configChat (conversation) {
-      this.zeus.input = conversation.message || conversation.html || conversation.text
-      this.zeus.filePrompt = (conversation.file_request !== undefined) && (conversation.file_request !== null)
-      if (conversation.possible_answers == undefined) {
-        this.zeus.suggestion = []
-      } else {
-        this.zeus.suggestion = JSON.parse(conversation.possible_answers) || []
-      }
-      this.numMessageSinceChatHistory += 1
-      this.setEnableUserConfirmation()
-      this.factType = conversation.fact_type || 'boolean'
-      this.user.input = null
-      this.user.isSent = false
-      this.user.disableInput = conversation.enforce_possible_answer
-    },
-    confirmBotResponse(confirmation) {
-      this.$http.post(this.api_url + 'store-user-confirmation', {
-        conversation_id: this.$localStorage.get('zeusId'),
-        confirmation: confirmation || false
-      }).then(response => {
-        this.zeus.enableUserConfirmation = false
-      }, response => {
-        this.zeus.enableUserConfirmation = true
-        this.connectionError = true
-      })
-    },
-    setEnableUserConfirmation() {
-      // Only start prompting once user has start responding to questions
-      this.zeus.enableUserConfirmation = this.numMessageSinceChatHistory + this.chatHistory.length > 4
-    },
-    resetChat () {
-      if (this.isLoggedIn) {
-        //TODO: some logout logic here
-      } else {
-        this.$localStorage.remove('zeusId')
-        this.$localStorage.remove('username')
-        this.$localStorage.remove('usertype')
-        this.$router.push('/')
+        this.numMessageSinceChatHistory += 1
+        this.setEnableUserConfirmation()
+        this.factType = conversation.fact_type || 'boolean'
+        this.user.input = null
+        this.user.isSent = false
+        this.user.disableInput = conversation.enforce_possible_answer
+      },
+      confirmBotResponse(confirmation) {
+        this.$http.post(this.api_url + 'store-user-confirmation', {
+          conversation_id: this.$localStorage.get('zeusId'),
+          confirmation: confirmation || false
+        }).then(response => {
+          this.zeus.enableUserConfirmation = false
+        }, response => {
+          this.zeus.enableUserConfirmation = true
+          this.connectionError = true
+        })
+      },
+      setEnableUserConfirmation() {
+        // Only start prompting once user has start responding to questions
+        this.zeus.enableUserConfirmation = this.numMessageSinceChatHistory + this.chatHistory.length > 4
+      },
+      resetChat() {
+        if (this.isLoggedIn) {
+          //TODO: some logout logic here
+        } else {
+          this.$localStorage.remove('zeusId')
+          this.$localStorage.remove('username')
+          this.$localStorage.remove('usertype')
+          this.$router.push('/')
+        }
       }
     }
   }
-}
 </script>
