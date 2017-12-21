23667f9b499357ce3fc8e284a7d74ee1c7d2c4f9
taimoorrana <taimoorrana1@gmail.com>
2017-12-21 18:17:34 -0500

    [#159] updated class and method names

--- src/ml_service/util/precedent_directory_cleaner_test.py
+++ src/ml_service/util/precedent_directory_cleaner_test.py
@@ -3,8 +3,8 @@ import util.precendent_directory_cleaner as directory_cleaner
 from util.constant import Path
 
 
-class TestLogger(unittest.TestCase):
+class PrecendentDirectoryCleanerTest(unittest.TestCase):
 
-    def test_write(self):
+    def test_english_precendent_removal(self):
         total_files_removed = directory_cleaner.remove_language_type_from_directory(Path.test_data_directory,"en")
         self.assertEqual(total_files_removed, 0)
