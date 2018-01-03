d98317253844b848dc9add764b043a0548a3a3c5
vynny <sylvain.czyzewski@gmail.com>
2018-01-03 17:07:57 -0500

    [#210] Refactored dummy fact method into a more permanent solution, returning only relevant facts for a claim category -Began refactoring ml service by adding list of all facts it expects at the endpoint

--- src/nlp_service/services/factService.py
+++ src/nlp_service/services/factService.py
@@ -1,4 +1,4 @@
-from postgresql_db.models import FactEntity
+from postgresql_db.models import FactEntity, Fact
 
 """
 Simulates the return values of the proposed ML service.
@@ -10,7 +10,7 @@ claim_category: The claim category determined from user input
 
 def submit_claim_category(claim_category):
     return {
-        'fact_id': dummy_next_fact(claim_category, [])
+        'fact_id': get_next_fact(claim_category, [])
     }
 
 
@@ -33,45 +33,37 @@ def submit_resolved_fact(conversation, current_fact, entity_value):
     facts_resolved = [fact_entity_row.fact.name for fact_entity_row in conversation.fact_entities]
 
     return {
-        'fact_id': dummy_next_fact(conversation.claim_category, facts_resolved)
+        'fact_id': get_next_fact(conversation.claim_category, facts_resolved)
     }
 
 
 """
-Dummy method that returns next fact given resolved facts. Stub implementation for
-proposed ML service.
+Returns next fact based on claim category given the resolved facts. 
 claim_category: claim category of the conversation
 facts_resolved: list of resolved fact keys
 :returns Next fact id to ask
 """
 
-
-def dummy_next_fact(claim_category, facts_resolved):
-    all_facts = [
+fact_mapping = {
+    "lease_termination": [
         "apartment_impropre",
-        "landlord_relocation_indemnity_fees",
         "landlord_rent_change",
         "landlord_retakes_apartment",
-        "landlord_sends_demand_regie_logement",
-        "tenant_lease_fixed",
         "tenant_bad_payment_habits",
-        "tenant_continuous_late_payment",
-        "tenant_individual_responsability",
         "tenant_left_without_paying",
         "tenant_monthly_payment",
         "tenant_owes_rent",
         "tenant_rent_not_paid_more_3_weeks",
-        "tenant_violence",
-    ]
+        "tenant_violence"
+    ],
+    "rent_change": [],
+    "nonpayment": [],
+    "deposits": []
+}
 
-    fact_dict = {
-        "lease_termination": all_facts,
-        "rent_change": all_facts,
-        "nonpayment": all_facts,
-        "deposits": all_facts
-    }
 
-    all_category_facts = fact_dict[claim_category.value.lower()]
+def get_next_fact(claim_category, facts_resolved):
+    all_category_facts = fact_mapping[claim_category.value.lower()]
     facts_unresolved = [fact for fact in all_category_facts if fact not in facts_resolved]
 
     # Pick the first unresolved fact, return None if none remain
