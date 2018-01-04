8c95b1cd2609d39fe5dae847e4b9f10d0f29ccb4
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2018-01-03 22:21:23 -0500

    [#234] remove landing page

--- src/beta_client/src/components/Beta.vue
+++ src/beta_client/src/components/Beta.vue
@@ -13,15 +13,13 @@
                 <p v-on:click="changeLanguage('en')" v-if="language != 'en'">ENGLISH</p>
             </div>
             <transition name="fade">
-                <div id="beta-page-title" v-bind:class="{ 'beta-page-title-in-chat': chat}">
+                <div id="beta-page-title">
                     <el-row>
                         <el-col :md="24">
                             <div id="title-image">
                                 <img alt="" src="../assets/beta_page_zeus_avatar.png" id="beta-page-zeus">
                                 <img alt="" src="../assets/beta_page_logo.png" id="beta-page-logo">
                             </div>
-                        </el-col>
-                        <el-col :md="24">
                             <div id="title-text">
                                 <h2 id="beta_page_description">A conflict prevention tool that uses artificial intelligence and case data to answer questions from landlords and tenants regarding the likely outcome of a dispute.</h2>
                             </div>
@@ -30,38 +28,25 @@
                 </div>
             </transition>
             <transition name="fade">
-                <div id="beta-page-sign-up" v-if="chat">
+                <div id="beta-page-sign-up">
                     <chat :language="language"></chat>
                 </div>
             </transition>
-            <transition name="fade">
-                <div id="beta-page-button-group">
-                    <el-row>
-                        <el-col :md="24">
-                            <div id="button-group">
-                                <el-button type="warning" v-if="!chat" v-on:click="chat = !chat">SIGN UP TO BETA</el-button>
-                            </div>
-                        </el-col>
-                    </el-row>
-                </div>
-            </transition>
         </div>
-        <transition name="fade">
-            <div id="beta-page-about-us" v-if="aboutUs">
-                <div id="beta-page-about-us-nav">
-                    <p v-on:click="aboutUs=!aboutUs">CLOSE</p>
-                </div>
-                <h2>Cyberjustice Laboratory</h2>
-                <img alt="" src="../assets/cjl.jpg" id="beta-page-zeus">
-                <div class="about-us-text">
-                    <p>The Cyberjustice Lab is a hub for thought and creativity, where justice processes are modelled and reimagined. More specifically, the Laboratory analyses the impact of technologies on justice and develops concrete technological tools that are adapted to the reality of justice systems.</p>
-                    <p>Hosted at the Faculty of Law of the Université de Montréal, its multidisciplinary international research team extends to 45 researchers from 23 institutions in Europe, the Americas, China and Australia, whose investigative goals tend towards improving the allocation and administration of judicial services so as to make optimal use of advances in technology.</p>
-                    <p>The Cyberjustice Laboratory is designing an artificial intelligence tool capable of assessing the validity of a litigant’s claim and proposing possible avenues for resolution.</p>
-                    <p>The solution takes the shape of a dynamic conversational agent designed to predict the likely outcomes of a variety of landlord/tenant disputes, based on personalized interactions with the disputant and analysis of relevant case law using machine learning algorithms.</p>
-                    <p>The project tests the boundaries of machine learning and natural language processing applied to the complex domain of law, and attempts to validate the utility of such a tool for stakeholders.</p>
-                </div>
+        <div id="beta-page-about-us" v-if="aboutUs">
+            <div id="beta-page-about-us-nav">
+                <p v-on:click="aboutUs=!aboutUs">CLOSE</p>
             </div>
-        </transition>
+            <h2>Cyberjustice Laboratory</h2>
+            <img alt="" src="../assets/cjl.jpg" id="beta-page-zeus">
+            <div class="about-us-text">
+                <p>The Cyberjustice Lab is a hub for thought and creativity, where justice processes are modelled and reimagined. More specifically, the Laboratory analyses the impact of technologies on justice and develops concrete technological tools that are adapted to the reality of justice systems.</p>
+                <p>Hosted at the Faculty of Law of the Université de Montréal, its multidisciplinary international research team extends to 45 researchers from 23 institutions in Europe, the Americas, China and Australia, whose investigative goals tend towards improving the allocation and administration of judicial services so as to make optimal use of advances in technology.</p>
+                <p>The Cyberjustice Laboratory is designing an artificial intelligence tool capable of assessing the validity of a litigant’s claim and proposing possible avenues for resolution.</p>
+                <p>The solution takes the shape of a dynamic conversational agent designed to predict the likely outcomes of a variety of landlord/tenant disputes, based on personalized interactions with the disputant and analysis of relevant case law using machine learning algorithms.</p>
+                <p>The project tests the boundaries of machine learning and natural language processing applied to the complex domain of law, and attempts to validate the utility of such a tool for stakeholders.</p>
+            </div>
+        </div>
     </el-container>
 </template>
 
@@ -74,7 +59,6 @@ export default {
     },
     data () {
         return {
-            chat: false,
             aboutUs: false,
             language: 'en'
         }
