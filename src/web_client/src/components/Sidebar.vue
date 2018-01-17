47fc587863bf67f0e6165c7115c5ce8ea5e8cec0
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2018-01-17 01:25:27 -0500

    [#260] update assets; finalize sidebar

--- src/web_client/src/components/Sidebar.vue
+++ src/web_client/src/components/Sidebar.vue
@@ -3,95 +3,92 @@
 </style>
 
 <template>
-    <div id="sidebar-component">
+    <div id="sidebar-component" v-bind:class="{ 'burger-menu': !openSidebar }">
         <!-- Menu Close -->
-        <div v-if="!openSidebar" v-on:click="openSidebar = true">
+        <div v-if="!openSidebar" v-on:click="openSidebar = true" id="sidebar-min">
             <img alt="" src="../assets/sidebar-toggle.png">
         </div>
         <!-- End of Menu Close -->
         <!-- Menu Open -->
-        <div v-if="openSidebar">
-            <!-- LOGO -->
-            <div id="sidebar-logo">
-                <img alt="" src="../assets/logo.png">
-                <p>Alpha</p>
-            </div>
-            <!-- End of LOGO -->
-            <!-- User Information -->
-            <div id="sidebar-account">
-                <h2>{{ username }}</h2>
-            </div>
-            <!-- End of User Information -->
-            <!-- Toggle Menu -->
-            <div id="sidebar-menu">
-                <!-- Uploaded File List -->
-                <div id="sidebar-upload-file" class="sidebar-menu" v-on:click="getFileList()" v-bind:class="{ 'active-menu': openFileList}">
-                    <h3>UPLOADED FILES <span>({{ uploadedFileList.length }})</span></h3>
+        <transition name="translate">
+            <div v-if="openSidebar" id="sidebar-full">
+                <!-- LOGO -->
+                <div id="sidebar-logo">
+                    <img alt="" src="../assets/logo.png" v-on:click="openSidebar = false">
+                    <p>Alpha</p>
                 </div>
-                <transition name="fade">
-                    <ul v-if="openFileList">
-                        <el-row>
-                            <li v-for="file in uploadedFileList">
-                                <el-col :sm="18">
-                                    <p>{{ file.name }}</p>
-                                </el-col>
-                                <el-col :sm="3" :offset="1">
-                                    <img class="sidebar-file-view" alt="" src="../assets/file_view.png">
-                                </el-col>
-                            </li>
-                        </el-row>
-                    </ul>
-                </transition>
-                <!-- End of Uploaded File List -->
-                <!-- Report List -->
-                <div id="sidebar-reports" class="sidebar-menu" v-on:click="openReportList = !openReportList; openFileList = false" v-bind:class="{ 'active-menu': openReportList}">
-                    <h3>REPORTS</h3>
+                <!-- End of LOGO -->
+                <!-- User Information -->
+                <div id="sidebar-account">
+                    <h2>{{ username }}</h2>
+                    <p>{{ usertype }}</p>
                 </div>
-                <transition name="fade">
-                    <ul v-if="openReportList">
-                        <el-row>
-                            <li>
-                                <el-col :sm="8" :offset="6">
-                                    <p>report.pdf</p>
-                                </el-col>
-                                <el-col :sm="2" :offset="2">
-                                    <img class="sidebar-file-view" alt="" src="../assets/file_view.png">
-                                </el-col>
-                            </li>
-                        </el-row>
-                    </ul>
-                </transition>
-                <!-- End Report List -->
-                <!-- Feedback -->
-                <div id="sidebar-feedback" class="sidebar-menu" @click="openFeedbackModal = true">
-                    <h3>FEEDBACK</h3>
+                <!-- End of User Information -->
+                <!-- Toggle Menu -->
+                <div id="sidebar-menu">
+                    <!-- Uploaded File List -->
+                    <div id="sidebar-upload-file" class="sidebar-menu" v-on:click="getFileList()" v-bind:class="{ 'active-menu': openFileList}">
+                        <h3>UPLOADED FILES <span>({{ uploadedFileList.length }})</span></h3>
+                    </div>
+                    <transition name="fade">
+                        <ul v-if="openFileList">
+                            <el-row>
+                                <li v-for="file in uploadedFileList">
+                                    <el-col :sm="18">
+                                        <p>{{ file.name }}</p>
+                                    </el-col>
+                                    <el-col :sm="3" :offset="1">
+                                        <img class="sidebar-file-view" alt="" src="../assets/file_view.png">
+                                    </el-col>
+                                </li>
+                            </el-row>
+                        </ul>
+                    </transition>
+                    <!-- End of Uploaded File List -->
+                    <!-- Report List -->
+                    <div id="sidebar-reports" class="sidebar-menu" v-on:click="openReportList = !openReportList; openFileList = false" v-bind:class="{ 'active-menu': openReportList}">
+                        <h3>REPORTS</h3>
+                    </div>
+                    <transition name="fade">
+                        <ul v-if="openReportList">
+                            <el-row>
+                                <li>
+                                    <el-col :sm="8" :offset="6">
+                                        <p>report.pdf</p>
+                                    </el-col>
+                                    <el-col :sm="2" :offset="2">
+                                        <img class="sidebar-file-view" alt="" src="../assets/file_view.png">
+                                    </el-col>
+                                </li>
+                            </el-row>
+                        </ul>
+                    </transition>
+                    <!-- End Report List -->
+                    <!-- Feedback -->
+                    <div id="sidebar-feedback" class="sidebar-menu" v-on:click="openFeedbackModal = true">
+                        <h3>FEEDBACK</h3>
+                    </div>
+                    <!-- End of Feedback -->
                 </div>
-                <!-- el-dialog for feedback -->
-                <el-dialog title="Feedback" :visible.sync="openFeedbackModal">
-                    <textarea id="feedback-text" v-model="feedback">
-                    </textarea>
-                    <span slot="footer" class="dialog-footer">
-                        <el-button @click="openFeedbackModal = false">Cancel</el-button>
-                        <el-button type="primary" @click="submitFeedback()">Submit</el-button>
-                    </span>
-                </el-dialog>
-                <!-- End of el-dialog for feedback -->
-                <!-- End of Feedback -->
-                <!-- Progress Bar -->
-                <div id="sidebar-progress">
+                <!-- End of Toggle Menu -->
+                <!-- Reset -->
+                <div id="sidebar-reset">
+                    <p v-on:click="resetChat()">RESET CONVERSATION</p>
                 </div>
-                <!-- End of Progress Bar -->
+                <!-- End of Reset -->
             </div>
-            <!-- End of Toggle Menu -->
-            <!-- Reset -->
-            <div id="sidebar-reset">
-                <p v-on:click="resetChat()">Reset</p>
-            </div>
-            <!-- End of Reset -->
-            <div id="toggle-bar">
-            </div>
-        </div>
+        </transition>
         <!-- End of Menu Open -->
+        <!-- el-dialog for feedback -->
+        <el-dialog title="Feedback" :visible.sync="openFeedbackModal">
+            <textarea id="feedback-input" v-model="feedback">
+            </textarea>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="openFeedbackModal = false">Cancel</el-button>
+                <el-button type="primary" @click="submitFeedback()">Submit</el-button>
+            </span>
+        </el-dialog>
+        <!-- End of el-dialog for feedback -->
     </div>
 </template>
 
@@ -104,7 +101,8 @@ export default {
             openReportList: false,
             openFeedbackModal: false,
             openSidebar: false,
-            username: this.$localStorage.get('username').toUpperCase(),
+            username: this.$localStorage.get('username'),
+            usertype: this.$localStorage.get('usertype'),
             feedback: '',
             //TODO: fetch username from conversation, now use usertype instead
             api_url: process.env.API_URL,
