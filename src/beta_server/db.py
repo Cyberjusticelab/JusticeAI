5947b89b42c05dfc63b9025f6c92dbc11697fdb2
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-01-18 19:06:53 -0500

    [#259] Updates Beta backend server to support storing whether a user is a legal professional

--- src/beta_server/db.py
+++ src/beta_server/db.py
@@ -12,7 +12,8 @@ class DbGateway:
             id text,
             question text,
             email text,
-            is_subscribed integer
+            is_subscribed integer,
+            is_legal_professional integer
         )''')
         self.conn.commit()
 
@@ -43,6 +44,15 @@ class DbGateway:
         self.conn.commit()
         return id
 
+    def insert_anonymous_legal_professional(self, is_legal_professional):
+        c = self.conn.cursor()
+        # prevent SQL injection with DB-API param substitution
+        id = str(uuid4())
+        insert = (id, str(is_legal_professional),)
+        c.execute('''INSERT INTO responses (id, is_legal_professional) VALUES (?,?)''', insert)
+        self.conn.commit()
+        return id
+
     def update_email_by_id(self, id, email):
         c = self.conn.cursor()
         # prevent SQL injection with DB-API param substitution
@@ -56,5 +66,13 @@ class DbGateway:
         # prevent SQL injection with DB-API param substitution
         insert = (int(is_subscribed), str(id),)
         c.execute('''UPDATE responses SET is_subscribed=? WHERE id=?''', insert)
+        self.conn.commit()
+        return id
+
+    def update_legal_professional_by_id(self, id, is_legal_professional):
+        c = self.conn.cursor()
+        # prevent SQL injection with DB-API param substitution
+        insert = (int(is_legal_professional), str(id),)
+        c.execute('''UPDATE responses SET is_legal_professional=? WHERE id=?''', insert)
         self.conn.commit()
         return id
