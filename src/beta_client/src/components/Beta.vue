22272f14138b7d2d21d4098a5bf16cf5a45f0556
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-01-18 19:57:00 -0500

    [#264] Changes the About Us text

--- src/beta_client/src/components/Beta.vue
+++ src/beta_client/src/components/Beta.vue
@@ -31,14 +31,11 @@
                 <div id="beta-page-about-us-nav">
                     <p v-on:click="aboutUs=!aboutUs">CLOSE</p>
                 </div>
-                <h2>Cyberjustice Laboratory</h2>
+                <h2>About the Cyberjustice Laboratory</h2>
                 <img alt="" src="../assets/cjl.jpg" id="beta-page-zeus">
                 <div class="about-us-text">
                     <p>The Cyberjustice Lab is a hub for thought and creativity, where justice processes are modelled and reimagined. More specifically, the Laboratory analyses the impact of technologies on justice and develops concrete technological tools that are adapted to the reality of justice systems.</p>
                     <p>Hosted at the Faculty of Law of the Université de Montréal, its multidisciplinary international research team extends to 45 researchers from 23 institutions in Europe, the Americas, China and Australia, whose investigative goals tend towards improving the allocation and administration of judicial services so as to make optimal use of advances in technology.</p>
-                    <p>The Cyberjustice Laboratory is designing an artificial intelligence tool capable of assessing the validity of a litigant’s claim and proposing possible avenues for resolution.</p>
-                    <p>The solution takes the shape of a dynamic conversational agent designed to predict the likely outcomes of a variety of landlord/tenant disputes, based on personalized interactions with the disputant and analysis of relevant case law using machine learning algorithms.</p>
-                    <p>The project tests the boundaries of machine learning and natural language processing applied to the complex domain of law, and attempts to validate the utility of such a tool for stakeholders.</p>
                 </div>
             </div>
         </transition>
