59a57f66ac9c332a57f859cda4cbd4137335b6e9
taimoorrana <taimoorrana1@gmail.com>
2018-01-22 22:01:09 -0500

    [#251] added function to convert date into unix time and find delta between 2 dates

--- src/ml_service/feature_extraction/post_processing/regex/regex_entity_extraction.py
+++ src/ml_service/feature_extraction/post_processing/regex/regex_entity_extraction.py
@@ -1,6 +1,8 @@
 from util.file import Load
 import re
 import enchant
+import datetime
+import time
 
 
 class EntityExtraction:
@@ -37,7 +39,7 @@ class EntityExtraction:
         for regex in regex_array:
             regex_result = regex.search(text)
             if regex_result:
-                sentence = regex_result.group(0)
+                sentence = regex_result.group(0).lower()
                 return EntityExtraction.__extract_regex_entity(sentence, regex_type)
         return False, 0
     
@@ -72,19 +74,40 @@ class EntityExtraction:
             return True, entity
 
         elif regex_type == 'DATE_REGEX':
-            sentence = sentence.lower()
             date_components = sentence.split(" ")
+
+            """
+            the value 3 is used to make sure we have all the required components. 
+            1 for the day, 1 for the month and 1 for the year.
+            """
             if len(date_components) == 3:
-                date_components[0] = int(re.sub(r"er|ere|em|eme", "", date_components[0]))
+                date_components[0] = re.sub(r"er|ere|em|eme", "", date_components[0])
                 try:
-                    date_components[1] = EntityExtraction.month_dict[date_components[1]]
+                    date_components[1] = str(EntityExtraction.month_dict[date_components[1]])
                 except KeyError:
-                    date_components[1] = EntityExtraction.month_dict[EntityExtraction.month_name_corrector(date_components[1])]
-                date_components[2] = int(date_components[2])
-                return True, 1  # temporary
+                    date_components[1] = str(EntityExtraction.month_dict[EntityExtraction.month_name_corrector(date_components[1])])
+                date_components[2] = date_components[2]
+                return True, EntityExtraction.__time_to_unix(date_components)
 
         return False, 0
     
     @staticmethod
-    def __time_to_unix(time):
-        pass
+    def __date_to_unix(date):
+        """
+        Given a date list (ex: [30,12,2019]) this function gets the unix time the represents this date
+        :param date: date to convert into unix time
+        :return: unix time representing the input date
+        """
+        date_string = " ".join(date)
+        unix_time = time.mktime(datetime.datetime.strptime(date_string, '%d %m %Y').timetuple())
+        return unix_time
+
+    @staticmethod
+    def __get_time_interval_in_days(first_date, second_date):
+        """
+        Calculates the time difference between 2 dates
+        :param first_date: date in unix time
+        :param second_date: date in unix time
+        :return: time difference between 2 dates
+        """
+        return abs(first_date - second_date) / EntityExtraction.one_day
