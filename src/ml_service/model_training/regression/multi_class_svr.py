c0ee1c45a9663b37d3028d0d2f877617e4163d4b
samuel-campbell <samuel.pcampbell@gmail.com>
2018-01-23 14:52:24 -0500

    [251] added svr and test code

--- src/ml_service/model_training/regression/multi_class_svr.py
+++ src/ml_service/model_training/regression/multi_class_svr.py
@@ -1,12 +1,14 @@
-import numpy as npr
 from sklearn.svm import SVR
+import numpy as np
+from sklearn.multioutput import MultiOutputRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.metrics import explained_variance_score
 from util.file import Load, Save
 from util.log import Log
-from feature_extraction.post_processing.regex.regex_tagger import TagPrecedents
 
 
-class MultiClassSVM:
+class MultiClassSVR:
 
     def __init__(self, data_set=None):
         """
@@ -33,26 +35,34 @@ class MultiClassSVM:
         """
         if self.model is not None:
             return self.model.coef_[0]
-        Log.write('Please train or load the classifier first')
+        Log.write('Please train or load the regression model first')
         return None
 
     def __test(self, x_test, y_test):
         """
-        UPDATE
+
         :param x_test:
         :param y_test:
         :return: None
         """
-        pass
+        Log.write("Testing Classifier")
+        y_predict = self.model.predict(x_test)
+        r_Score = r2_score(y_test, y_predict)
+        explained_variance = explained_variance_score(y_test, y_predict)
+        Log.write('R2: {}'.format(r_Score))
+        Log.write('Explained Variance: {}'.format(explained_variance))
 
     def train(self):
         """
-        CHANGE
+        Train a classifier using Linear SVC
+        1) reshape date in a format that sklearn understands
+        2) Binarize data for multi output
+        3) split training data
+        4) train (fit)
+        5) test model
         :return: None
         """
         x_total, y_total = self.reshape_dataset() # 1
-        self.mlb = MultiLabelBinarizer() # 2
-        y_total = self.mlb.fit_transform(y_total)
 
         x_train, x_test, y_train, y_test = train_test_split(
             x_total, y_total, test_size=0.20, random_state=42) # 3
@@ -60,28 +70,24 @@ class MultiClassSVM:
         Log.write("Sample size: {}".format(len(x_total)))
         Log.write("Train size: {}".format(len(x_train)))
         Log.write("Test size: {}".format(len(x_test)))
-        Log.write("Training Classifier Using Multi Class SVM")
+        Log.write("Training Regression Using Multi Class SVR")
 
-        clf = OneVsRestClassifier(SVC(kernel='linear', random_state=42)) # 4
+        clf = MultiOutputRegressor(SVR(kernel='linear')) # 4
         clf.fit(x_train, y_train)
         self.model = clf
         self.__test(x_test, y_test) # 5
 
     def save(self):
         """
-        CHANGE
+        Since the regression and classifier models are separate,
+        then new index will be assigned to each model.
+
+        1) Save classifier of each column into a binary format
+        2) Save the prediction model into binary
         :return:
         """
-        # ------------------- 1 -----------------------------
-        linear_labels = {}
-        indices = TagPrecedents().get_intent_indice()['outcomes_vector']
-        for i in range(len(self.mlb.classes_)):
-            linear_labels[i] = indices[self.mlb.classes_[i]][1]
         save = Save()
-        save.save_binary("classifier_labels.bin", linear_labels)
-
-        # ------------------- 2 -----------------------------
-        save.save_binary("multi_class_svm_model.bin", self.model)
+        save.save_binary("multi_class_svr_model.bin", self.model)
 
     def predict(self, data):
         """
@@ -93,13 +99,36 @@ class MultiClassSVM:
 
     def load(self):
         """
-        CHANGE THIS
+        Returns a model of the classifier
+        :return: OneVsRestClassifier(SVC())
         """
-        self.model = Load.load_binary("multi_class_svm_model.bin")
+        self.model = Load.load_binary("multi_class_svr_model.bin")
 
     def reshape_dataset(self):
         """
-        CHANGE THIS
+        Restructure the data to accomodate the sklearn library
+        1) Reshape the x data
+            1.1) 2D numpy array: [
+                    [precedent #1 facts],
+                    [precedent #2 facts],
+                    ...
+                ]
+
+        2) Reshape the y data
+            2.1) The data looks as such: [1, 1, 1, 0, 1, 0, 0, 1...]
+
+            2.2) We must create a new list with only the integer values greater
+                 than 1. We assume that 1/0 represent booleans
+
+            2.3) Example:        (transformation)
+                [1, 1, 554, 0, 1] ------------------> [554]
+
+            2.4) Create a 2D numpy array from the new list:[
+                [precedent #1 outcomes],
+                [precedent #2 outcomes],
+                ...
+            ]
+        :return: x_total <#1.1>, y_total <#2.4>
         """
 
         # --------------------1--------------------------
@@ -111,8 +140,8 @@ class MultiClassSVM:
         for precedent in self.data_set:
             classified_precedent = []
             for i in range(len(precedent['outcomes_vector'])):
-                if precedent['outcomes_vector'][i] == 1:
-                    classified_precedent.append(i)
+                if precedent['outcomes_vector'][i] > 1:
+                    classified_precedent.append(precedent['outcomes_vector'][i])
             y_list.append(classified_precedent)
         y_total = np.array(y_list)
         return x_total, y_total
---
+++ src/ml_service/model_training/regression/multi_class_svr_test.py
@@ -0,0 +1,63 @@
+import unittest
+from model_training.regression.multi_class_svr import MultiClassSVR
+import numpy as np
+
+
+class TestLinearSvc(unittest.TestCase):
+    precedent1 = {'facts_vector': [1, 0, 0, 0], 'outcomes_vector': [500, 1, 0]}
+    precedent2 = {'facts_vector': [0, 1, 0, 0], 'outcomes_vector': [254, 1, 0]}
+    precedent3 = {'facts_vector': [1, 1, 0, 0], 'outcomes_vector': [847, 0, 0]}
+    precedent4 = {'facts_vector': [0, 0, 1, 0], 'outcomes_vector': [28, 1, 1]}
+    precedent5 = {'facts_vector': [1, 0, 1, 0], 'outcomes_vector': [445, 1, 0]}
+    precedent6 = {'facts_vector': [0, 1, 1, 0], 'outcomes_vector': [120, 1, 0]}
+    precedent7 = {'facts_vector': [1, 1, 1, 0], 'outcomes_vector': [4, 0, 0]}
+    precedent8 = {'facts_vector': [0, 0, 0, 1], 'outcomes_vector': [592.1, 1, 0]}
+
+    data = [
+        precedent1,
+        precedent2,
+        precedent3,
+        precedent4,
+        precedent5,
+        precedent6,
+        precedent7,
+        precedent8
+    ]
+
+    def test_svc_train(self):
+        linear_svc = MultiClassSVR(self.data)
+        linear_svc.train()
+        data = np.array([1, 0, 0, 0])
+        expected_result = 500
+        predicted_result = linear_svc.predict(data)[0]
+        self.assertGreaterEqual(expected_result + (expected_result * 0.1), predicted_result[0])
+        self.assertLessEqual(expected_result - (expected_result * 0.1), predicted_result[0])
+
+    def test_reshape_dataset(self):
+        linear_svc = MultiClassSVR(self.data)
+        x, y = linear_svc.reshape_dataset()
+        x = x[0]
+        y = y[0]
+
+        expected_x =[
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 1, 0],
+            [1, 0, 1, 0],
+            [0, 1, 1, 0],
+            [1, 1, 1, 0],
+            [0, 0, 0, 1]
+        ]
+
+        expected_y = [[500], [254], [847], [28], [445], [120], [4], [592.1]]
+
+        for i in range(len(x)):
+            self.assertEqual(x[i], expected_x[0][i])
+
+        for i in range(len(y)):
+            self.assertEqual(y[i], expected_y[0][i])
+
+    def test_get_weights(self):
+        linear_svc = MultiClassSVR(self.data)
+        self.assertIsNone(linear_svc.get_weights())
