3a144558cc9287b9b56cd30f481f307ec3d093f2
vynny <sylvain.czyzewski@gmail.com>
2018-01-22 20:07:08 -0500

    [#280] Renames broken NLP integration test to something more accurate

--- src/nlp_service/rasa/nlp_integration_test.py
+++ src/nlp_service/rasa/nlp_integration_test.py
@@ -18,7 +18,7 @@ class TestNLPIntegration(unittest.TestCase):
         cls.rasaClassifier.train(force_train=True)
         cls.intentThreshold = IntentThreshold(min_percent_difference=0.3, min_confidence_threshold=0.4)
 
-    def test_claim_category_lease_termination(self):
+    def test_all_claim_category_text(self):
         filepath = os.getcwd() + '/rasa/data/category/claim_category.json'
         data = json.loads(codecs.open(filepath, 'r', 'utf-8').read().encode('utf-8'))
         example_objects = data['rasa_nlu_data']['common_examples']
