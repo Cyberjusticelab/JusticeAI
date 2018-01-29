4c1875b4d138bc50f3b844f45a88ff0dc683f5ba
taimoorrana <taimoorrana1@gmail.com>
2018-01-28 23:38:13 -0500

    [#251] regex test complete

--- src/ml_service/feature_extraction/post_processing/regex/regex_lib_test.py
+++ src/ml_service/feature_extraction/post_processing/regex/regex_lib_test.py
@@ -5,13 +5,12 @@ from feature_extraction.post_processing.regex.regex_lib import RegexLib
 from util.file import Load
 
 
-
 class RegexLibTest(unittest.TestCase):
 
     def setUp(self):
         self.regex_lib = RegexLib()
-        self.fact_cluster_regex_dict = Load().load_binary("cluster_regex_dict")
-        print("done")
+        self.fact_cluster_regex_dict = Load().load_binary("cluster_regex_dict.bin")
+
 
     def test_fact_regexes(self):
         for regex_name in self.fact_cluster_regex_dict.keys():
@@ -19,13 +18,20 @@ class RegexLibTest(unittest.TestCase):
             test_file_names = self.fact_cluster_regex_dict[regex_name]
             total_lines_matched = 0
             total_nb_lines_in_file = 0
-            for file_name in test_file_names:
-                file = open(Path.cluster_directory + 'fact/' + file_name, "r", encoding="utf-8")
+            file = open(Path.cluster_directory + 'fact/' + test_file_names[0], "r", encoding="utf-8")
+
+            for line in file:
+                if line == '\n' or '---' in line:
+                    break
+                total_nb_lines_in_file += 1
+            file.seek(0)
+            for regex in regexes:
                 for line in file:
-                    total_nb_lines_in_file += 1
-                    for regex in regexes:
-                        if regex.search(line):
-                            total_lines_matched += 1
-                file.close()
-            self.assertFalse(total_lines_matched > 0 and total_lines_matched / total_nb_lines_in_file > 0.5)
-        self.assertTrue(True)
+                    if line == '\n':
+                        break
+                    line = '[1] ' + line
+                    if regex.search(line):
+                        total_lines_matched += 1
+                file.seek(0)
+            file.close()
+            self.assertTrue(total_lines_matched > 0 and total_lines_matched / total_nb_lines_in_file > 0.5)
