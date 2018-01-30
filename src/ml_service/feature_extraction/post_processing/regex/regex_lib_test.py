27bae6ea977affa41108b4e8eaf0127baf31da8e
taimoorrana <taimoorrana1@gmail.com>
2018-01-29 20:26:08 -0500

    [#251] updated test to use the regex_lib_helper instead of regex_lib

--- src/ml_service/feature_extraction/post_processing/regex/regex_lib_test.py
+++ src/ml_service/feature_extraction/post_processing/regex/regex_lib_test.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 import unittest
 from util.constant import Path
-from feature_extraction.post_processing.regex.regex_lib import RegexLib
+from feature_extraction.post_processing.regex import regex_lib_helper
 from util.file import Load
 
 
 class RegexLibTest(unittest.TestCase):
 
     def setUp(self):
-        self.regex_lib = RegexLib()
         self.fact_cluster_regex_dict = Load().load_binary('cluster_regex_dict.bin')
 
     def test_fact_regexes(self):
         regex_types = ['fact', 'demand']
         for regex_type in regex_types:
             for regex_name in self.fact_cluster_regex_dict[regex_type].keys():
-                regexes = self.regex_lib.get_regexes(regex_name)
+                regexes = regex_lib_helper.get_regexes(regex_name)
                 test_file_names = self.fact_cluster_regex_dict[regex_type][regex_name]
                 total_lines_matched = 0
                 total_nb_lines_in_file = 0
