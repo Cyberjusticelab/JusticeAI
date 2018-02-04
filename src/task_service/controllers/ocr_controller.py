7b4cc797ac6a06dcf445c105bfcd4c5a602f8304
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-02-04 11:42:57 -0500

    [#25] Successfully installs and configures Tesseract and OpenCV, implements a proof of concept for extracting text from an image

--- src/task_service/controllers/ocr_controller.py
+++ src/task_service/controllers/ocr_controller.py
@@ -2,11 +2,12 @@ from flask import jsonify, abort, make_response
 
 from postgresql_db.models import *
 
-from task_service.app import db
-
 # Logging
 import logging
 import sys
+
+import pytesseract
+import cv2
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 log = logging.getLogger(__name__)
@@ -16,9 +17,10 @@ def extract_text(conversation_id, image_data):
     # TODO: document controller method
     """
     """
-    return jsonify({
-        'image_text': image_data
-    })
+    # TODO: use imdecode to pass image data directly instead of reading from disk
+    img = cv2.imread('/usr/src/app/src/task_service/test.png')
+    example_text = pytesseract.image_to_string(img, lang='eng')
 
-if __name__ == '__main__':
-    extract_text(1, 'this is image data')
+    return jsonify({
+        'image_text': example_text
+    })
