396bee149cd5a802cbadc6c52d33c7add25896fa
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-02-04 14:30:13 -0500

    [#25] Implements simplest case for OCR with tesseract+opencv+numpy as part of task_service

--- src/task_service/controllers/ocr_controller.py
+++ src/task_service/controllers/ocr_controller.py
@@ -8,6 +8,7 @@ import sys
 
 import pytesseract
 import cv2
+import numpy as np
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 log = logging.getLogger(__name__)
@@ -17,10 +18,19 @@ def extract_text(conversation_id, image_data):
     # TODO: document controller method
     """
     """
-    # TODO: use imdecode to pass image data directly instead of reading from disk
-    img = cv2.imread('/usr/src/app/src/task_service/test.png')
-    example_text = pytesseract.image_to_string(img, lang='eng')
-
+    img = _get_image_from_data(image_data)
+    text = _get_string_from_np_img(img)
     return jsonify({
-        'image_text': example_text
+        'image_text': text
     })
+
+def _get_image_from_file(file_path):
+    return cv2.imread(file_path)
+
+def _get_image_from_data(data):
+    np_data =  np.fromstring(data, np.uint8)
+    return cv2.imdecode(np_data, 0) # 0 indicates grayscale
+
+def _get_string_from_np_img(np_img):
+    return pytesseract.image_to_string(np_img, lang='eng')
+
