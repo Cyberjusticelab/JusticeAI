ebe11aa3df2e64054fd43db190edaa2851dc8f07
taimoorrana <taimoorrana1@gmail.com>
2018-02-05 15:10:37 -0500

    [#186] updated test

--- src/ml_service/util/precedent_directory_cleaner_test.py
+++ src/ml_service/util/precedent_directory_cleaner_test.py
@@ -5,5 +5,6 @@ from util.constant import Path
 
 class PrecendentDirectoryCleanerTest(unittest.TestCase):
     def test_precendent_removal(self):
-        total_files_removed = directory_cleaner.remove_files(Path.test_data_directory)
-        self.assertEqual(total_files_removed, ([], []))
+        total_files_removed = directory_cleaner.remove_files(Path.cluster_directory + 'fact/')
+        self.assertEqual(total_files_removed[0], 0)
+        self.assertEqual(total_files_removed[1], 56)
