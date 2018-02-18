780d6f832108337bcc1a5bd82b2309041541d1d6
vynny <sylvain.czyzewski@gmail.com>
2018-02-18 18:36:39 -0500

    [#346] Added cases to fact_service_test

--- src/nlp_service/services/fact_service_test.py
+++ src/nlp_service/services/fact_service_test.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import Mock
 
 from nlp_service.services import fact_service
-from postgresql_db.models import ClaimCategory, Conversation, PersonType, db, Fact, FactType
+from postgresql_db.models import ClaimCategory, Conversation, PersonType, db, Fact, FactType, FactEntity
 
 fact_service.ml_service.get_anti_facts = Mock(return_value={
     "not_violent": "violent",
@@ -49,7 +49,7 @@ class FactServiceTest(unittest.TestCase):
 
         self.assertIsNotNone(next_fact["fact_id"])
 
-    def get_next_fact(self):
+    def test_get_next_fact(self):
         conversation = Conversation(name="Bob", person_type=PersonType.TENANT,
                                     claim_category=ClaimCategory.LEASE_TERMINATION)
         db.session.add(conversation)
@@ -58,10 +58,44 @@ class FactServiceTest(unittest.TestCase):
         next_fact = fact_service.get_next_fact(conversation)
         self.assertIsNotNone(next_fact)
 
-    # def get_next_fact_all_resolved(self):
-    #     all_lease_termination_facts = list(fact_service.fact_mapping["lease_termination"])
-    #     next_fact = fact_service.get_next_fact(ClaimCategory.LEASE_TERMINATION, all_lease_termination_facts)
-    #     self.assertIsNone(next_fact)
+    def test_get_next_fact_all_resolved(self):
+        all_lease_termination_facts = fact_service.get_category_fact_list("lease_termination")
+
+        # Concat all the facts
+        all_facts = []
+        all_facts.extend(all_lease_termination_facts["facts"])
+        all_facts.extend(all_lease_termination_facts["additional_facts"])
+
+        # Define a conversation
+        conversation = Conversation(name="Bob", person_type=PersonType.TENANT,
+                                    claim_category=ClaimCategory.LEASE_TERMINATION)
+
+        # Add all facts to the conversation as resolved
+        for fact_key in all_facts:
+            fact = Fact.query.filter_by(name=fact_key).first()
+            fact_entity = FactEntity(fact=fact, value="false")
+            conversation.fact_entities.append(fact_entity)
+
+        db.session.add(conversation)
+        db.session.commit()
+
+        next_fact = fact_service.get_next_fact(conversation)
+        self.assertIsNone(next_fact)
+
+    def test_replace_anti_facts(self):
+        anti_fact_dict = {
+            "not_violent": "violent",
+            "tenant_individual_responsability": "tenant_group_responsability",
+            "tenant_lease_fixed": "tenant_lease_indeterminate",
+            "tenant_rent_not_paid_less_3_weeks": "tenant_rent_not_paid_more_3_weeks"
+        }
+
+        fact_list = ["not_violent", "tenant_lease_indeterminate", "tenant_rent_not_paid_more_3_weeks"]
+        expected_fact_list = ["violent", "tenant_lease_fixed", "tenant_rent_not_paid_more_3_weeks"]
+
+        processed_fact_list = fact_service.replace_anti_facts(fact_list, anti_fact_dict)
+
+        self.assertListEqual(processed_fact_list, expected_fact_list)
 
     def test_extract_fact_bool(self):
         intent = {'name': 'true', 'confidence': 0.90}
