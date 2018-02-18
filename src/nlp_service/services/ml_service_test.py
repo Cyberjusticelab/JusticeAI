35d0f1bdc1116f1a66708a7e630c019977f5d8f8
vynny <sylvain.czyzewski@gmail.com>
2018-02-18 16:56:23 -0500

    [#346] Fixed ml_service_test, added case to response_strings_test

--- src/nlp_service/services/ml_service_test.py
+++ src/nlp_service/services/ml_service_test.py
@@ -1,7 +1,14 @@
 import unittest
+from unittest import mock
 
 from nlp_service.services import ml_service
 from postgresql_db.models import db, Conversation, PersonType, Fact, FactEntity
+
+
+def mock_get_anti_facts():
+    return {
+        "tenant_rent_not_paid_more_3_weeks": "tenant_rent_not_paid_less_3_weeks"
+    }
 
 
 class MlServiceTest(unittest.TestCase):
@@ -23,24 +30,30 @@ class MlServiceTest(unittest.TestCase):
         prediction_dict = ml_service.extract_prediction("bad_key", mock_ml_response)
         self.assertFalse(bool(prediction_dict))
 
-    def test_generate_fact_dict(self):
+    @mock.patch('ml_service.get_anti_facts', side_effect=mock_get_anti_facts)
+    def test_generate_fact_dict(self, antifact_mock):
         conversation = Conversation(name="Bob", person_type=PersonType.TENANT)
         db.session.add(conversation)
         db.session.commit()
 
         fact1 = Fact.query.filter_by(name="apartment_dirty").first()
-        fact2 = Fact.query.filter_by(name="tenant_rent_not_paid_more_3_weeks").first()
-
         fact_entity_true = FactEntity(fact=fact1, value="true")
         conversation.fact_entities.append(fact_entity_true)
         db.session.commit()
 
+        fact2 = Fact.query.filter_by(name="tenant_rent_not_paid_more_3_weeks").first()
         fact_entity_false = FactEntity(fact=fact2, value="false")
         conversation.fact_entities.append(fact_entity_false)
         db.session.commit()
 
+        fact3 = Fact.query.filter_by(name="tenant_owes_rent").first()
+        fact_entity_money = FactEntity(fact=fact3, value="400.00")
+        conversation.fact_entities.append(fact_entity_money)
+        db.session.commit()
+
         fact_dict = ml_service.generate_fact_dict(conversation)
         self.assertTrue(len(fact_dict) != 0)
-
         self.assertTrue(fact_dict["apartment_dirty"] == 1)
-        self.assertTrue(fact_dict["tenant_rent_not_paid_more_3_weeks"] == 0)
+        self.assertTrue(fact_dict["tenant_rent_not_paid_more_3_weeks"] == 0)  # Fact
+        self.assertTrue(fact_dict["tenant_rent_not_paid_less_3_weeks"] == 1)  # Anti-fact
+        self.assertTrue(fact_dict["tenant_owes_rent"] == 400.00)  # Money fact
--- src/nlp_service/services/response_strings_test.py
+++ src/nlp_service/services/response_strings_test.py
@@ -53,3 +53,9 @@ class TestResponse(unittest.TestCase):
             }
         ])
         self.assertIn("AZ-11111", question)
+
+    def test_responses_prompt_additional(self):
+        prompt = Responses.prompt_additional_questions(5)
+        prompt = prompt.replace("|", "")  # Remove the pipe
+        self.assertTrue(prompt in self.responseInstance.prompt_additional)
+        self.assertTrue("5" in prompt)
