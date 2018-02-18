d56a3b96108c7c4e9d1758d76e67ad4103ee2de9
vynny <sylvain.czyzewski@gmail.com>
2018-02-18 18:21:12 -0500

    [#346] Added case to ml_service_test

--- src/nlp_service/services/ml_service_test.py
+++ src/nlp_service/services/ml_service_test.py
@@ -4,10 +4,6 @@ from unittest.mock import Mock
 from nlp_service.services import ml_service
 from postgresql_db.models import db, Conversation, PersonType, Fact, FactEntity
 
-ml_service.get_anti_facts = Mock(return_value={
-    "tenant_rent_not_paid_more_3_weeks": "tenant_rent_not_paid_less_3_weeks"
-})
-
 
 class MlServiceTest(unittest.TestCase):
     def test_extract_prediction(self):
@@ -28,7 +24,12 @@ class MlServiceTest(unittest.TestCase):
         prediction_dict = ml_service.extract_prediction("bad_key", mock_ml_response)
         self.assertFalse(bool(prediction_dict))
 
-    def test_generate_fact_dict(self):
+    def test_generate_fact_dict_tenant(self):
+        # Setup Mock
+        ml_service.get_anti_facts = Mock(return_value={
+            "tenant_rent_not_paid_more_3_weeks": "tenant_rent_not_paid_less_3_weeks"
+        })
+
         conversation = Conversation(name="Bob", person_type=PersonType.TENANT)
         db.session.add(conversation)
         db.session.commit()
@@ -50,6 +51,40 @@ class MlServiceTest(unittest.TestCase):
 
         fact_dict = ml_service.generate_fact_dict(conversation)
         self.assertTrue(len(fact_dict) != 0)
+        self.assertTrue(fact_dict["asker_is_tenant"] == 1)
+        self.assertTrue(fact_dict["apartment_dirty"] == 1)
+        self.assertTrue(fact_dict["tenant_rent_not_paid_more_3_weeks"] == 0)  # Fact
+        self.assertTrue(fact_dict["tenant_rent_not_paid_less_3_weeks"] == 1)  # Anti-fact
+        self.assertTrue(fact_dict["tenant_owes_rent"] == 400.00)  # Money fact
+
+    def test_generate_fact_dict_landlord(self):
+        # Setup Mock
+        ml_service.get_anti_facts = Mock(return_value={
+            "tenant_rent_not_paid_less_3_weeks": "tenant_rent_not_paid_more_3_weeks"
+        })
+
+        conversation = Conversation(name="Bob", person_type=PersonType.LANDLORD)
+        db.session.add(conversation)
+        db.session.commit()
+
+        fact1 = Fact.query.filter_by(name="apartment_dirty").first()
+        fact_entity_true = FactEntity(fact=fact1, value="true")
+        conversation.fact_entities.append(fact_entity_true)
+        db.session.commit()
+
+        fact2 = Fact.query.filter_by(name="tenant_rent_not_paid_more_3_weeks").first()
+        fact_entity_false = FactEntity(fact=fact2, value="false")
+        conversation.fact_entities.append(fact_entity_false)
+        db.session.commit()
+
+        fact3 = Fact.query.filter_by(name="tenant_owes_rent").first()
+        fact_entity_money = FactEntity(fact=fact3, value="400.00")
+        conversation.fact_entities.append(fact_entity_money)
+        db.session.commit()
+
+        fact_dict = ml_service.generate_fact_dict(conversation)
+        self.assertTrue(len(fact_dict) != 0)
+        self.assertTrue(fact_dict["asker_is_landlord"] == 1)
         self.assertTrue(fact_dict["apartment_dirty"] == 1)
         self.assertTrue(fact_dict["tenant_rent_not_paid_more_3_weeks"] == 0)  # Fact
         self.assertTrue(fact_dict["tenant_rent_not_paid_less_3_weeks"] == 1)  # Anti-fact
