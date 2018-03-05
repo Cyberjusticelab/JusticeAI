c6f2fe18159a1f5b2b7f701a3bda8b0a9b31fe11
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-03-05 08:58:57 -0500

    [#375] Implements display of latest SHA on Git master branch on landing page

--- src/web_client/config/dev.env.js
+++ src/web_client/config/dev.env.js
@@ -1,9 +1,10 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
-const { spawnSync } = require('child_process')
+var spawnSync = require('child_process').spawnSync
 
+var gitMasterSHA = spawnSync('git', ['rev-parse', 'origin/master'], { encoding: 'utf-8' }).stdout.trim()
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_URL: '"http://localhost:3003/"',
-  GIT_LATEST_MASTER_COMMIT: spawnSync('git', ['rev-parse', 'origin/master'])
+  GIT_LATEST_MASTER_COMMIT: '"' + gitMasterSHA + '"'
 })
