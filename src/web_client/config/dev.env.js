2e17b36dcd75d23becb2f65abfa2b65261bd0000
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-03-05 07:54:21 -0500

    [#375] Passes latest git SHA on master as environment variable

--- src/web_client/config/dev.env.js
+++ src/web_client/config/dev.env.js
@@ -1,7 +1,9 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
+const { spawnSync } = require('child_process')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:3003/"'
+  API_URL: '"http://localhost:3003/"',
+  GIT_LATEST_MASTER_COMMIT: spawnSync('git', ['rev-parse', 'origin/master'])
 })
