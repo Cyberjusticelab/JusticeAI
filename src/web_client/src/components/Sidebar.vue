829b9a0b93f09d14eb4d9ee310a429a3e8775000
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2018-03-10 14:58:12 -0500

    [#43] prep the logic for report generation; create mock payload

--- src/web_client/src/components/Sidebar.vue
+++ src/web_client/src/components/Sidebar.vue
@@ -1,36 +1,36 @@
-<style lang="scss" scoped>
-@import "../theme/Sidebar"
+<style lang='scss' scoped>
+@import '../theme/Sidebar'
 </style>
 
 <template>
-    <div id="sidebar-component" v-bind:class="{ 'burger-menu': !openSidebar }">
+    <div id='sidebar-component' v-bind:class='{ "burger-menu": !openSidebar }'>
         <!-- 1. Menu Close -->
-        <div v-if="!openSidebar" v-on:click="openSidebar = true" id="sidebar-min">
-            <img alt="" src="../assets/sidebar-toggle.png">
+        <div v-if='!openSidebar' v-on:click='view()' id='sidebar-min'>
+            <img alt='' src='../assets/sidebar-toggle.png'>
         </div>
         <!-- 1. End of Menu Close -->
         <!-- 2.1 Menu Open -->
-        <transition name="translate">
-            <div v-if="openSidebar && !openDashboard" id="sidebar-full">
+        <transition name='translate'>
+            <div v-if='openSidebar && !openDashboard' id='sidebar-full'>
                 <!-- LOGO -->
-                <div id="sidebar-logo">
-                    <img alt="" src="../assets/logo.png" v-on:click="openSidebar = false">
+                <div id='sidebar-logo'>
+                    <img alt='' src='../assets/logo.png' v-on:click='openSidebar = false'>
                     <p>Beta</p>
                 </div>
                 <!-- End of LOGO -->
                 <!-- Pending Info -->
-                <div id="sidebar-dashboard">
-                    <el-progress type="circle" :percentage="progress"></el-progress>
+                <div id='sidebar-info'>
+                    <el-progress type='circle' :percentage='progress'></el-progress>
                     <p>Provide more information to Zeus to get a prediction on your case</p>
                 </div>
                 <!-- End of Pending Info -->
                 <!-- Feedback -->
-                <div v-on:click="openFeedbackModal = true" class="sidebar-option">
+                <div v-on:click='openFeedbackModal = true' class='sidebar-option'>
                     <p>FEEDBACK</p>
                 </div>
                 <!-- End of Feedback -->
                 <!-- Reset -->
-                <div v-on:click="resetChat()" class="sidebar-option">
+                <div v-on:click='resetChat()' class='sidebar-option'>
                     <p>RESET CONVERSATION</p>
                 </div>
                 <!-- End of Reset -->
@@ -38,17 +38,19 @@
         </transition>
         <!-- 2.1 End of Menu Open -->
         <!-- 2.2 Stat Dashboard -->
-        <transition name="el-zoom-in-center">
-            <div v-if="openSidebar && openDashboard"
+        <transition name='el-zoom-in-center'>
+            <div v-if='openSidebar && openDashboard' id='sidebar-dashboard'>
+
+            </div>
         </transition>
         <!-- 2.2 End of Stat Dashboard -->
         <!-- el-dialog for feedback -->
-        <el-dialog title="Feedback" :visible.sync="openFeedbackModal">
-            <textarea id="feedback-input" v-model="feedback">
+        <el-dialog title='Feedback' :visible.sync='openFeedbackModal'>
+            <textarea id='feedback-input' v-model='feedback'>
             </textarea>
-            <span slot="footer" class="dialog-footer">
-                <el-button v-on:click="openFeedbackModal = false">Cancel</el-button>
-                <el-button type="primary" v-on:click="submitFeedback()">Submit</el-button>
+            <span slot='footer' class='dialog-footer'>
+                <el-button v-on:click='openFeedbackModal = false'>Cancel</el-button>
+                <el-button type='primary' v-on:click='submitFeedback()'>Submit</el-button>
             </span>
         </el-dialog>
         <!-- End of el-dialog for feedback -->
@@ -67,7 +69,67 @@ export default {
             usertype: this.$localStorage.get('usertype'),
             feedback: '',
             progress: 0,
-            //TODO: fetch username from conversation, now use usertype instead
+            //TODO: set report to new object and modify by report api callback. Now mock data.
+            //This is the expected payload format
+            report: {
+                accuracy: 98,
+                data_set: 350000,
+                similar_case: 50,
+                curves: {
+                    legal_fees: ['whatever data is needed to create a bell curve']
+                },
+                outcome: {
+                    lease_termination: 1,
+                    order_resiliation: 1,
+                    apartment_impropre: 1,
+                    legal_fees: 82,
+                    additional_indemnity_fees: 1000
+                },
+                similar_precedents: [
+                    {
+                        precedent: 'AZ-1111111',
+                        facts: {
+                            absent : 1,
+                            apartment_impropre : 0,
+                            apartment_infestation : 1,
+                            asker_is_landlord : 1,
+                            asker_is_tenant : 1,
+                            bothers_others : 1,
+                            disrespect_previous_judgement : 1
+                        },
+                        outcomes: {
+                            absent : 1,
+                            apartment_impropre : 0,
+                            apartment_infestation : 0,
+                            asker_is_landlord : 0,
+                            asker_is_tenant : 1,
+                            bothers_others : 1,
+                            disrespect_previous_judgement : 1
+                        }
+                    },
+                    {
+                        precedent: 'AZ-222222',
+                        facts: {
+                            absent : 1,
+                            apartment_impropre : 0,
+                            apartment_infestation : 1,
+                            asker_is_landlord : 1,
+                            asker_is_tenant : 1,
+                            bothers_others : 1,
+                            disrespect_previous_judgement : 1
+                        },
+                        outcomes: {
+                            absent : 1,
+                            apartment_impropre : 0,
+                            apartment_infestation : 0,
+                            asker_is_landlord : 0,
+                            asker_is_tenant : 1,
+                            bothers_others : 1,
+                            disrespect_previous_judgement : 1
+                        }
+                    }
+                ]
+            },
             api_url: process.env.API_URL,
             connectionError: false
         }
@@ -80,25 +142,33 @@ export default {
         })
     },
     methods: {
-        submitFeedback(){
+        view () {
+            // TODO: do some black magic here to call report endpoint
+            this.openSidebar = true
+            this.openDashboard = true // TODO: remove this dev code. change to true for testing dashboard UI
+            if (this.openDashboard) {
+
+            }
+        },
+        submitFeedback () {
             if (this.feedback) {
                 this.$http.post(this.api_url + 'feedback',{
                     feedback: this.feedback
                 }).then(
                     response => {
-                        alert("Thank you for providing us with feedback!");
+                        alert('Thank you for providing us with feedback!');
                         this.openFeedbackModal = false;
                         this.feedback = '';
                     },
                     response => {
                         this.connectionError = true
-                        alert("Sorry, an error occurred and we could not receive your feedback.");
+                        alert('Sorry, an error occurred and we could not receive your feedback.');
                         this.openFeedbackModal = false;
                         this.feedback = '';
                     }
                 )
             } else {
-                alert("No feedback entered");
+                alert('No feedback entered');
             }
         },
         resetChat () {
