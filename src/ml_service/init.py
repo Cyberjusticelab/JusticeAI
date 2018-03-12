42fd085eb004388ad1d40870aaee862ff3cb23a4
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-03-12 09:40:18 -0400

    [#380] Refactors ML binary downloader, using wget instead

--- src/ml_service/init.py
+++ src/ml_service/init.py
@@ -1,12 +1,10 @@
 import os
 import time
 import nltk
-import requests
-from sys import stdout
-from threading import Thread
+from sys import exit
 from util.constant import Path
 from util.log import Log
-import threading
+import subprocess
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -15,59 +13,37 @@ nltk.download('perluniprops')
 ADDRESS_INDEX = 0
 FILE_SIZE_INDEX = 1
 
-sem = threading.Semaphore()
 
-binary_names_dict = {
-    'classifier_labels': ['https://capstone.cyberjustice.ca/data/bin/classifier_labels.bin', 638],
-    'additional_indemnity_money_regressor': ['https://capstone.cyberjustice.ca/data/bin/additional_indemnity_money_regressor.bin', 478368],
-    'additional_indemnity_money_scaler': ['https://capstone.cyberjustice.ca/data/bin/additional_indemnity_money_scaler.bin', 1250],
-    'multi_class_svm_model': ['https://capstone.cyberjustice.ca/data/bin/multi_class_svm_model.bin', 20739501],
-    'model_metrics': ['https://capstone.cyberjustice.ca/data/bin/model_metrics.bin', 2170],
-    'tenant_pays_landlord_regressor': ['https://capstone.cyberjustice.ca/data/bin/tenant_pays_landlord_regressor.bin', 478584],
-    'tenant_pays_landlord_scaler': ['https://capstone.cyberjustice.ca/data/bin/tenant_pays_landlord_scaler.bin', 1250],
-    'similarity_case_numbers': ['https://capstone.cyberjustice.ca/data/bin/similarity_case_numbers.bin', 999398],
-    'similarity_model': ['https://capstone.cyberjustice.ca/data/bin/similarity_model.bin', 31335546],
-    'similarity_scaler': ['https://capstone.cyberjustice.ca/data/bin/similarity_scaler.bin', 1586]
-}
+binary_urls = [
+    'https://capstone.cyberjustice.ca/data/bin/classifier_labels.bin',
+    'https://capstone.cyberjustice.ca/data/bin/additional_indemnity_money_regressor.bin',
+    'https://capstone.cyberjustice.ca/data/bin/additional_indemnity_money_scaler.bin',
+    'https://capstone.cyberjustice.ca/data/bin/multi_class_svm_model.bin',
+    'https://capstone.cyberjustice.ca/data/bin/model_metrics.bin',
+    'https://capstone.cyberjustice.ca/data/bin/tenant_pays_landlord_regressor.bin',
+    'https://capstone.cyberjustice.ca/data/bin/tenant_pays_landlord_scaler.bin',
+    'https://capstone.cyberjustice.ca/data/bin/similarity_case_numbers.bin',
+    'https://capstone.cyberjustice.ca/data/bin/similarity_model.bin',
+    'https://capstone.cyberjustice.ca/data/bin/similarity_scaler.bin'
+]
 
+for binary_url in binary_urls:
+    binary_name = binary_url.split('/')[-1]
+    abs_file_path = os.path.join(Path.binary_directory, binary_name)
 
-def monitor_download(filename, filesize):
-    sem.acquire()
-    time.sleep(2)
-    current_size = 0
-    limit = filesize * 0.99
-    while limit > current_size:
-        current_size = os.stat(filename).st_size
-        percentage = (current_size / filesize * 1.00) * 100
-        percentage = float("{0:.2f}".format(percentage, 2))
-        stdout.write("\r%s - Download percentage: %.2f%%" %
-                     (filename, percentage))
-        stdout.flush()
-        time.sleep(3)
-    Log.write("\n[END] Downloaded {} Binary".format(filename))
-    sem.release()
-
-
-for binary_name in binary_names_dict:
-    # Checks if file exists before downloading
-    rel_path = binary_name + '.bin'
-    abs_file_path = os.path.join(Path.binary_directory, rel_path)
-
-    if not os.path.exists(abs_file_path):
-        Log.write("[START] Downloading {} binary".format(binary_name))
-        thr = Thread(target=monitor_download, args=(abs_file_path, binary_names_dict[binary_name][FILE_SIZE_INDEX]))
-        thr.start()
-
-        response = requests.get(
-            binary_names_dict[binary_name][ADDRESS_INDEX],
-            auth=(
-                os.environ['CJL_USER'],
-                os.environ['CJL_PASS']),
-            stream=True,
-            verify=False)
-        with open(abs_file_path, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-    else:
+    if os.path.exists(abs_file_path):
         Log.write("{} binary file requirement already satisfied.".format(binary_name))
+        continue
+
+    if (not os.environ['CJL_USER']) or (not os.environ['CJL_PASS']):
+        Log.write("You must supply the CJL_USER and CJL_PASS environment variables to download binaries from capstone.cyberjustice.ca")
+        exit(1)
+
+    Log.write(binary_name)
+    status = subprocess.call("wget --quiet --recursive --force-directories --show-progress --progress=bar:force --no-cache --user={} --password={} --output-document={} {}".format(
+        os.environ['CJL_USER'], os.environ['CJL_PASS'], abs_file_path, binary_url
+    ).split(" "))
+
+    if status != 0:
+        Log.write("Non-zero status! {}".format(status))
+        exit(1)
