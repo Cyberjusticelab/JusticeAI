843ed4d6699f34cf859b9ae6e293672c040a46f2
vynny <sylvain.czyzewski@gmail.com>
2018-03-12 16:14:01 -0400

    [#43] Converts all the strings that are numerical into ints in the report

--- src/nlp_service/services/report_service.py
+++ src/nlp_service/services/report_service.py
@@ -19,13 +19,21 @@ def generate_report(conversation, ml_prediction, similar_precedents):
     report['curves'] = {}
 
     possible_curve_outcomes = ml_statistics['regressor']
-    for outcome in ml_prediction.keys():
+    for outcome in ml_prediction:
         if outcome in possible_curve_outcomes:
             report['curves'][outcome] = dict(possible_curve_outcomes[outcome])
             report['curves'][outcome]['outcome_value'] = ml_prediction[outcome]
 
     # Outcomes
-    report['outcomes'] = dict(ml_prediction)
+    report['outcomes'] = {}
+
+    for outcome in ml_prediction:
+        if int(ml_prediction[outcome]) == 1:
+            report['outcomes'][outcome] = True
+        elif int(ml_prediction[outcome]) == 0:
+            report['outcomes'][outcome] = False
+        else:
+            report['outcomes'][outcome] = ml_prediction[outcome]
 
     # Similar Precedents
     report['similar_precedents'] = []
@@ -33,9 +41,19 @@ def generate_report(conversation, ml_prediction, similar_precedents):
     # Filter out all facts and outcomes that don't matter from precedents
     for precedent in similar_precedents:
         filtered_fact_dict = {k: v for k, v in precedent['facts'].items() if k in conversation_facts}
-        precedent['facts'] = filtered_fact_dict
+        precedent['facts'] = __dict_values_to_int(filtered_fact_dict)
         filtered_outcome_dict = {k: v for k, v in precedent['outcomes'].items() if k in ml_prediction}
-        precedent['outcomes'] = filtered_outcome_dict
+        precedent['outcomes'] = __dict_values_to_int(filtered_outcome_dict)
         report['similar_precedents'].append(precedent)
 
     return report
+
+
+def __dict_values_to_int(dict):
+    for key in dict:
+        if type(dict[key]) is str:
+            try:
+                dict[key] = int(float(dict[key]))
+            except ValueError:
+                pass
+    return dict
