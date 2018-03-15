0e70d7f86a6b881c17bf520dd476f51046de2371
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-03-15 15:41:10 -0400

    [#43] Removes VueChart dependency, no longer needed

--- src/web_client/src/main.js
+++ src/web_client/src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import VueResource from 'vue-resource'
 import VueLocalStorage from 'vue-localstorage'
 import VueUpload from 'vue-upload-component'
-import VueChart from 'vuechart';
 import ElementUI from 'element-ui'
 import Icon from 'vue-awesome/components/Icon'
 import 'vue-awesome/icons'
@@ -14,7 +13,6 @@ Vue.config.productionTip = false
 Vue.use(VueResource)
 Vue.use(VueLocalStorage)
 Vue.use(ElementUI)
-Vue.component(VueChart.name, VueChart);
 Vue.component('icon', Icon)
 Vue.component('file-upload', VueUpload)
 
