f02a6571733ec3269bd409f2178bda97ef40bd1c
vynny <sylvain.czyzewski@gmail.com>
2018-03-28 15:48:09 -0400

    [#369] Fixed broken ml test that sam didnt tell me about

--- src/ml_service/init_test.py
+++ src/ml_service/init_test.py
@@ -6,7 +6,7 @@ from util.constant import Path
 class TestInitScript(unittest.TestCase):
     def test_init(self):
         import init
-        self.assertTrue(os.path.isfile(Path.binary_directory + 'tenant_pays_landlord_scaler.bin'))
-        self.assertTrue(os.path.isfile(Path.binary_directory + 'tenant_pays_landlord_regressor.bin'))
+        self.assertTrue(os.path.isfile(Path.binary_directory + 'tenant_ordered_to_pay_landlord_scaler.bin'))
+        self.assertTrue(os.path.isfile(Path.binary_directory + 'tenant_ordered_to_pay_landlord_regressor.bin'))
         self.assertTrue(os.path.isfile(Path.binary_directory + 'multi_class_svm_model.bin'))
         self.assertTrue(os.path.isfile(Path.binary_directory + 'classifier_labels.bin'))
