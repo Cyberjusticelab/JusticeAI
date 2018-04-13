94a9e9fc220fb894d788a2cce0a931a451929529
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2018-04-13 05:02:52 -0400

    [#405] update readme

--- src/web_client/config/dev.env.js
+++ src/web_client/config/dev.env.js
@@ -5,6 +5,6 @@ var spawnSync = require('child_process').spawnSync
 var gitMasterSHA = spawnSync('git', ['rev-parse', 'origin/master'], { encoding: 'utf-8' }).stdout.trim()
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:3003/"',
+  API_URL: '"http://192.168.1.131:3003/"',
   GIT_LATEST_MASTER_COMMIT: '"' + gitMasterSHA + '"'
 })
